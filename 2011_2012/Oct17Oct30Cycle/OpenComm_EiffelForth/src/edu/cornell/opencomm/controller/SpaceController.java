1234eabc6a39e3016696db73d1d85ea84f1cddcf
Risa Naka <rn96@cornell.edu>
2011-10-28 12:55:25 -0400

    Updated SpaceController and other constants in Network
    Signed-off-by: Risa Naka <rn96@cornell.edu>

--- 2011_2012/Oct17Oct30Cycle/OpenComm_EiffelForth/src/edu/cornell/opencomm/controller/SpaceController.java
+++ 2011_2012/Oct17Oct30Cycle/OpenComm_EiffelForth/src/edu/cornell/opencomm/controller/SpaceController.java
@@ -1,7 +1,9 @@
 package edu.cornell.opencomm.controller;
 
+import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.muc.InvitationRejectionListener;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 import org.jivesoftware.smackx.muc.Occupant;
@@ -37,6 +39,7 @@ public class SpaceController {
 		this.muc = this.space.getMUC();
 		this.muc.addParticipantStatusListener(this.configParticipantStatusListener());
 		this.muc.addInvitationRejectionListener(this.configInvitationRejectionListener());
+		this.muc.addMessageListener(this.configMessageListener());
 	} // end SpaceController method
 
 	
@@ -108,7 +111,7 @@ public class SpaceController {
 	 * =============================================================================================
 	 * =============================================================================================
 	 * 
-	 * CONFIGURATION OF LISTENERS:
+	 * CUSTOM LISTENERS:
 	 * receiveInvitationRequest
 	 * confirmInvitationRequest
 	 * rejectInvitationRequest
@@ -117,8 +120,6 @@ public class SpaceController {
 	 * confirmKickoutRequest
 	 * rejectKickoutRequest
 	 * receiveKickoutRequestRejection
-	 * configInvitationRejectionListener
-	 * configParticipantStatusListener
 	 * 
 	 * ===========================================
 	 * ===========================================
@@ -191,19 +192,51 @@ public class SpaceController {
 		// TODO Risa - receive rejection of invitation request
 	}
 	
-	public void receiveKickoutRequest() {
-		// TODO Risa - receive kickout request
+	public String[] receiveKickoutRequest(String kickoutRequest) {
+		// Check the kickoutRequest is accurate
+		if (kickoutRequest.contains(Network.REQUEST_KICKOUT)) {
+			// extract kickout request info
+			String kickoutRequestInfo = kickoutRequest.split(Network.REQUEST_KICKOUT)[0];
+			String requester = (kickoutRequestInfo.split("@requester")[0]).split("@kickee")[0];
+			String kickee = (kickoutRequestInfo.split("@requester" + requester + "@kickee")[0]).split("@reason")[0];
+			String reason = (kickoutRequestInfo.split("@reason").length == 0 ? Network.DEFAULT_KICKOUT : kickoutRequestInfo.split("@reason")[0]);
+			String[] kickoutInfo = {requester, kickee, reason};
+			// DEBUG
+			if (D) Log.d(TAG, "receiveKickoutRequest - received kickout request from " 
+					+ kickoutInfo[0] + " for room " + this.space.getRoomID() + " for user "
+					+ kickoutInfo[1] + ": reason - " + kickoutInfo[2]);
+			return kickoutInfo;
+		}
+		// else
+		Log.e(TAG, "receiveInvitationRequest - incorrectly called");
+		return null;
 	}
-	public void confirmKickoutRequest() {
+	public void confirmKickoutRequest(String[] kickoutInfo) {
 		// TODO Risa - confirm kickout request
 	}
-	public void rejectKickoutRequest() {
+	public void rejectKickoutRequest(String[] kickoutInfo, String reason) {
 		// TODO Risa - reject kickout request
 	}
 	public void receiveKickoutRequestRejection() {
 		// TODO Risa - receive rejection of kickout request
 	}
 	
+	/** END CUSTOM LISTENERS
+	 * 
+	 * =============================================================================================
+	 * =============================================================================================
+	 * =============================================================================================
+	 * =============================================================================================
+	 * 
+	 * CONFIGURATION OF LISTENERS:
+	 * 
+	 * configInvitationRejectionListener
+	 * configParticipantStatusListener
+	 * configMessageListener
+	 * 
+	 * ===========================================
+	 * ===========================================
+	 */
 	private InvitationRejectionListener configInvitationRejectionListener() {
 		InvitationRejectionListener inviteRejectListener = new InvitationRejectionListener() {
 
@@ -563,6 +596,82 @@ public class SpaceController {
 		};
 		return participantStatListener;
 	} // configParticipantStatusListener method
+	
+	/** = configuration of message listener */
+	public PacketListener configMessageListener() {
+		PacketListener messageListener = new PacketListener() {
+			/** Process the messages coming in in packets */
+			public void processPacket(Packet packet) {
+				Message message = (Message) packet;
+				String from = message.getFrom();
+				String body = message.getBody();
+				// if the primary user is the owner of the room
+				Occupant userOcc = muc.getOccupant(MainApplication.user_primary.getUsername() 
+						+ "/" + MainApplication.user_primary.getNickname());
+				// if the primary user is the room's owner
+				if (userOcc.getAffiliation().equals(Network.ROLE_OWNER)) {
+					// if it is a invitation request:
+					if (body.contains(Network.REQUEST_INVITE)) {
+						if (D) Log.d(TAG, "configMessageListener - invitation request received");
+						String[] inviteInfo = receiveInvitationRequest(body);
+						// TODO - UI: some form of confirmation or rejection
+						boolean userDecision = true;
+						if (userDecision) {
+							// invite user to the room
+							confirmInvitationRequest(inviteInfo);
+						}
+						else {
+							// Reject the invitation request
+							// TODO - UI: permit user to type reason for rejection
+							String reason = Network.DEFAULT_REJECT;
+							rejectInvitationRequest(inviteInfo, reason);
+						}
+					}
+					// if it is a kickout request:
+					else if (body.contains(Network.REQUEST_KICKOUT)) {
+						if (D) Log.d(TAG, "configMessageListener - kickout request received");
+						String[] kickoutInfo = receiveKickoutRequest(body);
+						// TODO - UI: some form of confirmation or rejection
+						boolean userDecision = true;
+						if (userDecision) {
+							// kickout user from room
+							confirmKickoutRequest(kickoutInfo);
+						}
+						else {
+							// Reject the kickout request
+							// TODO - UI: permit user to type reason for rejection
+							String reason = Network.DEFAULT_REJECT;
+							rejectKickoutRequest(kickoutInfo, reason);
+						}
+					}
+					// normal messages
+					else {
+						if (D) Log.d(TAG, "Regular message received");
+					}
+				}
+				// if the primary user is not the owner of the room
+				else {
+					// listen for rejection of invitation request
+					if (body.contains(Network.REJECT_INVITE)) {
+						
+					}
+					// listen for rejection of invitation request
+					else if (body.contains(Network.REJECT_INVITE)) {
+						
+					}
+					// normal messages
+					else {
+						if (D) Log.d(TAG, "Regular message received");
+					}
+				}
+				
+
+				
+			}
+		};
+		return messageListener;
+		
+	}
 	
 	/** END CONFIGURATION OF LISTENERS
 	 * 
