6e190eb2c3842703c9a73a594283ae0f1bf919a3
Nora N-Q <ncn27@cornell.edu>
2011-11-12 04:01:19 -0500

    Trying to cleanup some classes
    Signed-off-by: Nora N-Q <ncn27@cornell.edu>

--- 2011_2012/Oct31Nov13Cycle/Software/Code_NewStructure/src/edu/cornell/opencomm/controller/ConfirmationController.java
+++ 2011_2012/Oct31Nov13Cycle/Software/Code_NewStructure/src/edu/cornell/opencomm/controller/ConfirmationController.java
@@ -5,21 +5,32 @@ import edu.cornell.opencomm.view.ConfirmationView;
 import edu.cornell.opencomm.R;
 import edu.cornell.opencomm.Values;
 import edu.cornell.opencomm.R.color;
-
+/** Handles the actions performed on an confirmation popup screen
+ * 
+ * @author vinay
+ *
+ */
 public class ConfirmationController {
 
 	private ConfirmationView confirmationView = null;
 	public ConfirmationController(ConfirmationView confirmationView) {
 		this.confirmationView = confirmationView;
 	}
+	
+	/** Handle when the window is pressed */
 	public void handlePopupWindowClicked() {
+		// Dismisses the window for now
 		confirmationView.getWindow().dismiss();		
 	}
+	
+	/** Handle when the cancel button is pressed */
 	public void handleAcceptButtonHover() {
+		// Turn button grey for now
 		confirmationView.getAcceptButton().setBackgroundColor(R.color.light_grey);
-		
 	}
+	/** Handle when the cancel button is pressed */
 	public void handleCancelButtonHover() {
+		// Turn button grey for now
 		confirmationView.getCancelButton().setBackgroundColor(R.color.light_grey);
 	}
 }
