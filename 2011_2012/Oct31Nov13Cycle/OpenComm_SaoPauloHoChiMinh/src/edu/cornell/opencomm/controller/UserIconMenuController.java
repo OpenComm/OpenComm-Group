53bc1524214dcf01010f4f9abef701eed0db24a4
Nora N-Q <ncn27@cornell.edu>
2011-11-16 20:06:04 -0500

    Deleting users should work
    Signed-off-by: Nora N-Q <ncn27@cornell.edu>

--- 2011_2012/Oct31Nov13Cycle/OpenComm_SaoPauloHoChiMinh/src/edu/cornell/opencomm/controller/UserIconMenuController.java
+++ 2011_2012/Oct31Nov13Cycle/OpenComm_SaoPauloHoChiMinh/src/edu/cornell/opencomm/controller/UserIconMenuController.java
@@ -1,11 +1,14 @@
 package edu.cornell.opencomm.controller;
 
+import org.jivesoftware.smack.XMPPException;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.util.Log;
 import edu.cornell.opencomm.Values;
 import edu.cornell.opencomm.view.SpaceView;
+import edu.cornell.opencomm.view.UserView;
 
 /** When a user press and hold an icon of the mainscreen, this controller
  *  is invoked. It displays a dialog menu.
@@ -14,8 +17,8 @@ import edu.cornell.opencomm.view.SpaceView;
  */  
 public class UserIconMenuController {
 
-	
-	private SpaceView spaceView = null;
+	private static UserView userView = null;
+	private static SpaceView spaceView = null;
 	private static Context context;
 
 	/** Constructor: UserIconMenuController */
@@ -26,7 +29,8 @@ public class UserIconMenuController {
 
 	/** Show a menu after long-pressing on a user icon (UserView)
 	 * Menu options are: Delete User, Cancel */
-	public static void showIconMenu(){
+	public static void showIconMenu(UserView uv){
+		userView = uv;
 		if (context == null)
 			Log.v("User Icon Menu", "NULLL");
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
@@ -35,6 +39,12 @@ public class UserIconMenuController {
 		    public void onClick(DialogInterface dialog, int item) {
 		    	if (Values.userviewMenu[item].equals("Delete User")){
 		    		//Do something if user clicks on Delete User
+		    		try{
+		    		spaceView.getSpace().getKickoutController().kickoutUser(userView.getPerson(), "Because you should leave");
+		    		} catch(XMPPException e){
+		    			Log.d("UserIconMenuController", "Couldn't kick out user " + userView.getPerson() + " from space " 
+		    					+ spaceView.getSpace().getRoomID());
+		    		}
 		    	}
 		    	else if (Values.userviewMenu[item].equals("Cancel")){
 		    		//Do something if user clicks on Cancel
