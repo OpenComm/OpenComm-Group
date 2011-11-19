ce38bf240707511335bb4ec1d7e5b60d48682916
Risa Naka <rn96@cornell.edu>
2011-11-19 16:22:34 -0500

    LoginView bottom bar click fixed for functionality and UI
    Signed-off-by: Risa Naka <rn96@cornell.edu>

--- 2011_2012/Oct31Nov13Cycle/OpenComm_SaoPauloHoChiMinh/src/edu/cornell/opencomm/view/LoginView.java
+++ 2011_2012/Oct31Nov13Cycle/OpenComm_SaoPauloHoChiMinh/src/edu/cornell/opencomm/view/LoginView.java
@@ -1,36 +1,16 @@
 package edu.cornell.opencomm.view;
 
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
-
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import edu.cornell.opencomm.R;
-import edu.cornell.opencomm.R.layout;
-import edu.cornell.opencomm.Values;
-import edu.cornell.opencomm.controller.ConfirmationController;
 import edu.cornell.opencomm.controller.LoginController;
-import edu.cornell.opencomm.controller.MainApplication;
-import edu.cornell.opencomm.model.Space;
-import edu.cornell.opencomm.network.Network;
-import edu.cornell.opencomm.network.NetworkService;
+
 
 public class LoginView extends Activity {
 	private static String LOG_TAG = "OC_LoginView"; // for error checking
@@ -58,6 +38,7 @@ public class LoginView extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_layout);
 
+		this.inflater = this.getLayoutInflater();
 		usernameEdit = (EditText) findViewById(R.id.editTextUsername);
 		passwordEdit = (EditText) findViewById(R.id.editTextPassword);
 		loginText = (TextView) findViewById(R.id.textLogin);
