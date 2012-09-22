b297c01f3725f9e540e92cf0ff0db2511f8577c8
Heming Ge <hwg33@cornell.edu>
2012-09-22 16:53:45 -0400

    Tutorial_Heming

---
+++ 2012_2013/Sept17Cycle/FrontEnd/Tutorials/Tutorial_Heming/MyFirstApp_Heming/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,41 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_display_message);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_display_message, menu);
+        return true;
+    }
+
+    
+/*    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+*/
+}
