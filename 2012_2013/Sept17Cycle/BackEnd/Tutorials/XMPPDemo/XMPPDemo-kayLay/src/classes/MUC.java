0d11478177ecf9aa4e246ab84cba5da721275e74
Kevin Lei <lei.kevin.n@gmail.com>
2012-09-23 23:55:10 -0400

    XMPP Demo
    The demo is finished. A picture of its implementation is shown. I am
    able to connect to the server, login and engage in a multi user chat
    which is printed to the console as the person type.

--- 2012_2013/Sept17Cycle/BackEnd/Tutorials/XMPPDemo/XMPPDemo-kayLay/src/classes/MUC.java
+++ 2012_2013/Sept17Cycle/BackEnd/Tutorials/XMPPDemo/XMPPDemo-kayLay/src/classes/MUC.java
@@ -3,12 +3,13 @@ package classes;
 
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smackx.Form;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
 
 /**This class is going to have basic chatting settings and Multi User Chat Capabilities*/
-public class MUC {
+public class MUC{
 	
 
 	MultiUserChat Muc;
@@ -16,10 +17,22 @@ public class MUC {
 
 	
 	
-	public MUC(Connection connection, String roomName) throws XMPPException{
-		Muc= new MultiUserChat(connection.getXMPPConnection(), roomName);
-		Muc.create(roomName);
-		Muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+	public MUC(Connection connection, String roomName, String nickName){
+		
+		try {
+			Muc= new MultiUserChat(connection.getXMPPConnection(), roomName);
+			Muc.create(nickName);
+			Muc.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+
+
+		} catch (XMPPException e) {
+			joinRoom(connection, roomName);
+			System.out.println("MultiUserConnection doesn't work");
+			e.printStackTrace();
+		}
+
+		
+
 	}
 	
 	public void joinRoom(Connection connection, String roomName){
@@ -27,6 +40,53 @@ public class MUC {
 			Muc.join(roomName);
 		} catch (XMPPException e) {
 			System.out.println("Failed joining room");
+		}
+		
+	}
+
+	public void sendMessage(String message){
+
+			try {
+				Muc.sendMessage(message);
+				
+			} catch (XMPPException e) {
+				System.out.println("error in the send message method body");
+				e.printStackTrace();
+			}
+
+		
+	}
+	
+	public void invite(String username){
+		Muc.invite(username, "Lei's room");
+	}
+	
+	public MultiUserChat getMUC(){
+		return Muc;
+	}
+	
+	public void listenForMessage(){
+		Muc.addMessageListener(new PacketListener(){
+			public void processPacket(Packet packet) {
+				
+				
+				String XML=packet.toXML();
+				int boundaryOne=XML.indexOf("<body>");
+				int boundaryTwo=XML.indexOf("</body>");
+				
+				String message=XML.substring(boundaryOne+6, boundaryTwo);
+				
+				System.out.println(message);
+			}
+		});
+	}
+	
+	public void getMessages(){
+		try{
+		  System.out.println("INTERFACE:" + Muc.nextMessage(1).getBody());
+		}
+		catch(NullPointerException npe){
+			
 		}
 		
 	}
