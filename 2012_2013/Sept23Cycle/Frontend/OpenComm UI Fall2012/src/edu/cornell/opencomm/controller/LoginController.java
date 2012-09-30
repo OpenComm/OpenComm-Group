2af071e0a908f3d01dd4e80454660c9cf539ab38
Ankit <as2536@cornell.edu>
2012-09-30 11:58:09 -0400

    Skeleton Code For Login Page

---
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/controller/LoginController.java
@@ -0,0 +1,68 @@
+package edu.cornell.opencomm.controller;
+
+import android.os.AsyncTask;
+import android.widget.EditText;
+import edu.cornell.opencomm.view.LoginView;
+
+public class LoginController  {
+
+	/**
+	 * The View Object
+	 */
+	private LoginView loginView;
+
+	/**
+	 * The enum defining the opcodes for Login Task
+	 *
+	 */
+	private enum ReturnState{SUCEEDED, COULDNT_CONNECT, WRONG_PASSWORD, ALREADY_CLICKED};
+
+
+
+	/**
+	 * @param view
+	 */
+	public LoginController(LoginView view){
+		this.loginView = view;
+	}
+
+
+	/**
+	 * @param usernameEdit
+	 * @param passwordEdit
+	 */
+	public void handleLoginButtonClick(final EditText userEmail, final EditText passwordEdit) {
+		//TODO exectue the login task 
+
+
+	}
+
+
+	private class LoginTask extends AsyncTask<String, Void, ReturnState> {
+
+
+
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			// TODO Ask the design team.
+			super.onProgressUpdate(values);
+		}
+		//[TODO] Establish a connection to the server and authenticate the username and password
+		//ReturnState specifies the outcome of the background task.
+		@Override
+		protected ReturnState doInBackground(String... strings) {
+			return null;
+		}
+
+		//[TODO] This function needs to do either
+		// 1. launch dashboard
+		// 2. go to last activity
+		// 3. login the user
+		@Override
+		protected void onPostExecute(ReturnState state) {
+
+		}
+	}
+
+}
