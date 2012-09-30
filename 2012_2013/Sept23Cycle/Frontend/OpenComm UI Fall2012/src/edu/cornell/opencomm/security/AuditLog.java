cb4d5e5737c47a1cedbeda3f0346492feb5bd01d
ae243 <ae243@cornell.edu>
2012-09-30 12:19:05 -0400

    skeleton code and package for security

---
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/security/AuditLog.java
@@ -0,0 +1,23 @@
+package edu.cornell.opencomm.security;
+
+/**
+ * 
+ * Handles all writing to the audit log.  Should not ever read the audit log.
+ *
+ */
+public class AuditLog {
+
+	/**
+	 * TODO: Creates and/or opens the audit log file.
+	 */
+	public AuditLog(){
+	}
+	
+	/**
+	 * TODO: Write the message to the log.  This needs synchronization primitives.
+	 * @param s the message to write
+	 */
+	public void writeAuditLog(String s){
+	}
+	
+}
---
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/security/AuthorizationCheck.java
@@ -0,0 +1,57 @@
+package edu.cornell.opencomm.security;
+
+/**
+ * 
+ * Use to check if users are allowed to do what they are requesting to do.
+ *
+ */
+public class AuthorizationCheck {
+
+	/**
+	 * Possible events that require authorization checks.
+	 */
+	public static final int CAN_CREATE_CONFERENCE = 0;
+	public static final int CAN_INVITE_USERS_TO_MAIN = 1;
+	public static final int CAN_INVITE_USERS_TO_SIDE = 2;
+	public static final int CAN_REMOVE_USERS_FROM_MAIN = 3;
+	public static final int CAN_REMOVE_USERS_FROM_SIDE = 4;
+	public static final int CAN_UPDATE_INFO = 5;
+	public static final int CAN_JOIN_CONFERENCE = 6;
+	public static final int CAN_PASS_MODERATOR_PRIVILEGES = 7;
+	public static final int CAN_LEAVE_CONFERENCE_MAIN = 8;
+	public static final int CAN_LEAVE_CONFERENCE_SIDE = 9;
+	
+	/**
+	 * TODO: checks whether or not the user has the credentials/privileges that are 
+	 * 		required for the corresponding action.
+	 * TODO: More parameters will be required for implementation
+	 * @param event the event that the user is trying to execute
+	 * @return a boolean true if the user is allowed to perform the event
+	 * 		or false if the user is not allowed to perform the event
+	 */
+	public boolean checkAuthRules(int event){
+		switch(event){
+		case CAN_CREATE_CONFERENCE:
+			break;
+		case CAN_INVITE_USERS_TO_MAIN:
+			break;
+		case CAN_INVITE_USERS_TO_SIDE:
+			break;
+		case CAN_REMOVE_USERS_FROM_MAIN:
+			break;
+		case CAN_REMOVE_USERS_FROM_SIDE:
+			break;
+		case CAN_UPDATE_INFO:
+			break;
+		case CAN_JOIN_CONFERENCE:
+			break;
+		case CAN_PASS_MODERATOR_PRIVILEGES:
+			break;
+		case CAN_LEAVE_CONFERENCE_MAIN:
+			break;
+		case CAN_LEAVE_CONFERENCE_SIDE:
+			break;
+		}
+		return false;
+	}
+}
