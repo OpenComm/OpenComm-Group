222ed06c21e5d42747e310cddd4ee96489282fab
Risa Naka <rn96@cs.cornell.edu>
2012-09-30 13:00:07 -0400

    remove string from android manifest

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/gen/edu/cornell/opencomm/R.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/gen/edu/cornell/opencomm/R.java
@@ -32,45 +32,50 @@ public final class R {
         public static final int separator_bar=0x7f020008;
     }
     public static final class id {
-        public static final int acceptConfirmOverlay=0x7f070015;
-        public static final int buttonAcceptConfirm=0x7f070011;
-        public static final int buttonCancelConfirm=0x7f070013;
-        public static final int cancelConfirmOverlay=0x7f070016;
-        public static final int confirmBar=0x7f07000f;
-        public static final int confirmBarFrame=0x7f07000e;
-        public static final int confirmOverlay=0x7f070014;
-        public static final int editText1=0x7f07001a;
-        public static final int editTextEmail=0x7f070001;
-        public static final int editTextPassword=0x7f070003;
-        public static final int emailBox=0x7f070018;
-        public static final int entireScreen=0x7f070007;
-        public static final int fullFrame=0x7f070017;
-        public static final int imageAcceptConfirm=0x7f070010;
-        public static final int imageCancelConfirm=0x7f070012;
-        public static final int imageView1=0x7f070020;
-        public static final int infoConfirm=0x7f070009;
-        public static final int loginButton=0x7f070005;
-        public static final int loginText=0x7f070006;
-        public static final int quickContactBadge1=0x7f07000c;
-        public static final int resetButton=0x7f07001d;
-        public static final int resetButtonImage=0x7f07001e;
-        public static final int resetPasswordText=0x7f07001f;
-        public static final int separatorBar=0x7f070021;
-        public static final int signUp=0x7f07001b;
-        public static final int signUpText=0x7f07001c;
-        public static final int signupButton=0x7f070004;
-        public static final int textLabelConfirm=0x7f070008;
-        public static final int textLabelEmail=0x7f070000;
-        public static final int textLabelPassword=0x7f070002;
-        public static final int textView1=0x7f070019;
-        public static final int textViewConfTitle=0x7f07000d;
-        public static final int textViewHeader=0x7f07000a;
-        public static final int textViewInfo=0x7f07000b;
+        public static final int acceptConfirmOverlay=0x7f080015;
+        public static final int buttonAcceptConfirm=0x7f080011;
+        public static final int buttonCancelConfirm=0x7f080013;
+        public static final int cancelConfirmOverlay=0x7f080016;
+        public static final int confirmBar=0x7f08000f;
+        public static final int confirmBarFrame=0x7f08000e;
+        public static final int confirmOverlay=0x7f080014;
+        public static final int editText1=0x7f08001a;
+        public static final int editTextEmail=0x7f080001;
+        public static final int editTextPassword=0x7f080003;
+        public static final int emailBox=0x7f080018;
+        public static final int entireScreen=0x7f080007;
+        public static final int fullFrame=0x7f080017;
+        public static final int imageAcceptConfirm=0x7f080010;
+        public static final int imageCancelConfirm=0x7f080012;
+        public static final int imageView1=0x7f080020;
+        public static final int infoConfirm=0x7f080009;
+        public static final int loginButton=0x7f080005;
+        public static final int loginText=0x7f080006;
+        public static final int menu_settings=0x7f080022;
+        public static final int quickContactBadge1=0x7f08000c;
+        public static final int resetButton=0x7f08001d;
+        public static final int resetButtonImage=0x7f08001e;
+        public static final int resetPasswordText=0x7f08001f;
+        public static final int separatorBar=0x7f080021;
+        public static final int signUp=0x7f08001b;
+        public static final int signUpText=0x7f08001c;
+        public static final int signupButton=0x7f080004;
+        public static final int textLabelConfirm=0x7f080008;
+        public static final int textLabelEmail=0x7f080000;
+        public static final int textLabelPassword=0x7f080002;
+        public static final int textView1=0x7f080019;
+        public static final int textViewConfTitle=0x7f08000d;
+        public static final int textViewHeader=0x7f08000a;
+        public static final int textViewInfo=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_login_view=0x7f030000;
-        public static final int confirm_layout=0x7f030001;
-        public static final int reset_password_layout=0x7f030002;
+        public static final int activity_signup_view=0x7f030001;
+        public static final int confirm_layout=0x7f030002;
+        public static final int reset_password_layout=0x7f030003;
+    }
+    public static final class menu {
+        public static final int activity_signup_view=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
