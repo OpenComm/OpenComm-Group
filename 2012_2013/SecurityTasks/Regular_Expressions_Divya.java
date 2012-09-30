54487bd07bf494ee2cef0c4b234a9f9eba73c625
ae243 <ae243@cornell.edu>
2012-09-30 19:50:39 -0400

    added SecurityTasks

---
+++ 2012_2013/SecurityTasks/Regular_Expressions_Divya.java
@@ -0,0 +1,31 @@
+package regex;
+
+public class Regular_Expressions_Divya {
+
+	/**
+	 * TODO: Make sure parameter is an syntactically name or title (they have the same rules).
+	 * 		It must abide by the following rules:
+	 * 			-Only contain characters
+	 * 			-Only the first letter of each word can be capitalized
+	 * @param nameOrTitle the name/title to verify
+	 * @return true if it is a valid name/title, false if it is not a valid name/title
+	 */
+	public boolean isValidNameOrTitle(String nameOrTitle){
+		return false;
+	}
+	
+	/**
+	 * TODO: Look through the java security and cryptography packages to implement hashing 
+	 * 		a password.  The basic concept of password hashing is getting a one-way function
+	 * 		(for example md5 - this is built into the java libraries), applying it to the 
+	 * 		password and returning the result of that function.  We will be using the built-in
+	 * 		java libraries because they are more reliable than any function we can implement, so 
+	 * 		please become more familiar with these libraries.
+	 * @param pass the password
+	 * @return the hashed value
+	 */
+	public String hashPass(String pass){
+		return null;
+	}
+	
+}
