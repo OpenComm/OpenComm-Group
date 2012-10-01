da0cacd0c719b82adf38460bf40a9c9204191585
Ankit <as2536@cornell.edu>
2012-10-01 18:30:08 -0400

    Allignment changes for Signup screen

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/StartAppActivity.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/StartAppActivity.java
@@ -5,60 +5,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import edu.cornell.opencomm.R;
+import edu.cornell.opencomm.controller.FontSetter;
 import edu.cornell.opencomm.model.ReturnState;
 
 public class StartAppActivity extends Activity {
-	 /**
+	/**
 	 * The TAG for logging
 	 */
-	private static final String TAG = StartAppActivity.class.getSimpleName();	
-	 /**
+	private static final String TAG = StartAppActivity.class.getSimpleName();
+	/**
 	 * The delay for splash screen in milliseconds
 	 */
 	private static final int SPLASH_SCREEN_DELAY = 3000;
-	
-    /* (non-Javadoc)
-     * @see android.app.Activity#onCreate(android.os.Bundle)
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_view);
-       
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-          public void run() {
-           connect();
-          }
-        }, SPLASH_SCREEN_DELAY);
-      
-    }
-    /**
-     * TODO: Use Network Service to connect to the server 
-     * Note: Netowrk service is a AsyncTask so there is no need to create another. Just Saying :)
-     * 
-     */
-    private void connect(){
-    	//TODO : Fix it Connection code here
-    	ReturnState returnState = ReturnState.SUCEEDED;
-    	if(returnState == ReturnState.SUCEEDED){
-    		launchLoginView();
-    	}else{
-    		//TODO : Alternate flow yet to decided
-    		onConnectionError();
-    	}
-    }
-    /**
-     * Finishes the current activity and launches the Login Activity
-     */
-    private void launchLoginView(){
-    	Intent intent = new Intent(StartAppActivity.this, LoginView.class);
-    	startActivity(intent);
-    }
-    /**
-     * TODO: Ask design team for flow if connection to server fails
-     */
-    private void onConnectionError(){
-    	//TODO
-    }
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_view);
+		FontSetter.applySanSerifFont(StartAppActivity.this,
+				findViewById(R.layout.activity_splash_view));
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				connect();
+			}
+		}, SPLASH_SCREEN_DELAY);
+
+	}
+
+	/**
+	 * TODO: Use Network Service to connect to the server Note: Netowrk service
+	 * is a AsyncTask so there is no need to create another. Just Saying :)
+	 * 
+	 */
+	private void connect() {
+		// TODO : Fix it Connection code here
+		ReturnState returnState = ReturnState.SUCEEDED;
+		if (returnState == ReturnState.SUCEEDED) {
+			launchLoginView();
+		} else {
+			// TODO : Alternate flow yet to decided
+			onConnectionError();
+		}
+	}
+
+	/**
+	 * Finishes the current activity and launches the Login Activity
+	 */
+	private void launchLoginView() {
+		Intent intent = new Intent(StartAppActivity.this, LoginView.class);
+		startActivity(intent);
+	}
+
+	/**
+	 * TODO: Ask design team for flow if connection to server fails
+	 */
+	private void onConnectionError() {
+		// TODO
+	}
 }
