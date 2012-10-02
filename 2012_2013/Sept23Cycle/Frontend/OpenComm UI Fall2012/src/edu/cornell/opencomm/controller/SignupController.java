65c9d32262d2f0a74920816ca23e7dda32006d12
Ankit <as2536@cornell.edu>
2012-10-01 20:44:52 -0400

    Comitting intermidiate changes for Signup
    Added NotificationView.java

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/controller/SignupController.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/controller/SignupController.java
@@ -4,16 +4,73 @@ import java.util.ArrayList;
 
 import org.apache.http.NameValuePair;
 
+import edu.cornell.opencomm.view.SignupView;
+
 import android.os.AsyncTask;
+import android.view.View;
 
 public class SignupController {
-	
+	/**
+	 * The Vie
+	 */
+	private SignupView view;
+	/**The constructor
+	 * @param view
+	 */
+	public SignupController(SignupView view){
+		this.view = view;
+	}
 	//TODO
 	//1. Handle save button
 	//2. Handle focus change on the textbox- guard condition to regain focus
 	//3. Handle cancel button
 	//4. Handle the android back button, and consume everthing else
+	
 
+	public void handleFirstNameFocusChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleLastNameFocusChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handlePasswordFocusChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleConfirmPasswordFocusChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleTitleFocusChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handlePhotoButtonClick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleSave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleCancel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleEmailFoucsChange(View view, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		
+	}
 	private class CreateUser extends AsyncTask<ArrayList<NameValuePair>, Void, Boolean> {
 
 		//TODO send the request to the server to create a new user
@@ -30,5 +87,4 @@ public class SignupController {
 			super.onPostExecute(result);
 		}
 	}
-
 }
--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/SignupView.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/SignupView.java
@@ -2,18 +2,209 @@ package edu.cornell.opencomm.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import edu.cornell.opencomm.R;
+import edu.cornell.opencomm.controller.FontSetter;
+import edu.cornell.opencomm.controller.SignupController;
 
 public class SignupView extends Activity {
+	/**
+	 * The constant for save click
+	 */
+	public static final int SAVE_CLICK = 1;
+	/**
+	 * The constant for cancel click
+	 */
+	public static final int CANCEL_CLICK = 2;
+	/**
+	 * The controller for signup
+	 */
+	private SignupController controller;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //TODO
-        setContentView(R.layout.activity_signup_view);
-    }
-    //TODO: Create and register onclick and focus change listner
-    //TODO: handle on save, on cancel
-    //TODO: kill the overlay
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signup_view);
+		FontSetter.applySanSerifFont(SignupView.this, findViewById(R.layout.activity_signup_view));
+		init();
+	}
+
+	/**
+	 * Initialize the signup view
+	 */
+	private void init() {
+		initFirstNameFocusChangeListner();
+		initLastNameFocusChangeListner();
+		initPasswordChangeListner();
+		initConfirmPasswordChangeListner();
+		initEmailFocusChangeListner();
+		initTitleFocusChangeListner();
+		initPhotoButtonClickEvent();
+		initSaveButtonClickEvent();
+		initSaveImageClickEvent();
+		initCancelButtonClickEvent();
+		initCancelImageClickEvent();
+	}
+
+	/**
+	 * Initialize the FirstName box
+	 */
+	private void initFirstNameFocusChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handleFirstNameFocusChange(view, hasFocus);
+			}
+		};
+		View firstNameBox = findViewById(R.id.firstNameBox);
+		firstNameBox.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * Initialize the Last Name box
+	 */
+	private void initLastNameFocusChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handleLastNameFocusChange(view, hasFocus);
+			}
+		};
+		View lastName = findViewById(R.id.lastNameBox);
+		lastName.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * Initialize email box
+	 */
+	private void initEmailFocusChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handleEmailFoucsChange(view, hasFocus);
+			}
+		};
+		View email = findViewById(R.id.emailBox);
+		email.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * Initialize the Password box
+	 */
+	private void initPasswordChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handlePasswordFocusChange(view, hasFocus);
+			}
+		};
+		View password = findViewById(R.id.passwordBox);
+		password.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * Initialize the confirm password box
+	 */
+	private void initConfirmPasswordChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handleConfirmPasswordFocusChange(view, hasFocus);
+			}
+		};
+		View confirmPassword = findViewById(R.id.confirmPassword);
+		confirmPassword.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * Initialize the title box
+	 */
+	private void initTitleFocusChangeListner() {
+		OnFocusChangeListener listner = new View.OnFocusChangeListener() {
+			public void onFocusChange(View view, boolean hasFocus) {
+				controller.handleTitleFocusChange(view, hasFocus);
+			}
+		};
+		View title = findViewById(R.id.titleBox);
+		title.setOnFocusChangeListener(listner);
+	}
+
+	/**
+	 * initialize photo button click event listner
+	 */
+	private void initPhotoButtonClickEvent() {
+		OnClickListener listner = new View.OnClickListener() {
+
+			public void onClick(View v) {
+				controller.handlePhotoButtonClick();
+
+			}
+		};
+		View photoButton = findViewById(R.id.photoButton);
+		photoButton.setOnClickListener(listner);
+	}
+
+	/**
+	 * initialize save button click event listner
+	 */
+	private void initSaveButtonClickEvent() {
+		OnClickListener listner = new View.OnClickListener() {
+
+			public void onClick(View v) {
+				controller.handleSave();
+
+			}
+		};
+		View saveButton = findViewById(R.id.saveButton);
+		saveButton.setOnClickListener(listner);
+	}
+
+	/**
+	 * initialize save image click event listner
+	 */
+	private void initSaveImageClickEvent() {
+		OnClickListener listner = new View.OnClickListener() {
+
+			public void onClick(View v) {
+				controller.handleSave();
+
+			}
+		};
+		View saveButton = findViewById(R.id.saveImage);
+		saveButton.setOnClickListener(listner);
+	}
+
+	/**
+	 * initialize save button click event listner
+	 */
+	private void initCancelButtonClickEvent() {
+		OnClickListener listner = new View.OnClickListener() {
+
+			public void onClick(View v) {
+				controller.handleCancel();
+
+			}
+		};
+		View saveButton = findViewById(R.id.cancelButton);
+		saveButton.setOnClickListener(listner);
+	}
+
+	/**
+	 * initialize save image click event listner
+	 */
+	private void initCancelImageClickEvent() {
+		OnClickListener listner = new View.OnClickListener() {
+
+			public void onClick(View v) {
+				controller.handleCancel();
+
+			}
+		};
+		View saveButton = findViewById(R.id.cancelImage);
+		saveButton.setOnClickListener(listner);
+	}
 
 }
