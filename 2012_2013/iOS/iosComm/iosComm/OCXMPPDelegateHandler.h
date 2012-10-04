641d8fac4274fe944c3e233209986e2c647cb665
Kevin Chen <chen.kevin.f@gmail.com>
2012-10-04 17:59:37 -0400

    i forgot to prefix the new files with OC. added that now

---
+++ 2012_2013/iOS/iosComm/iosComm/OCXMPPDelegateHandler.h
@@ -0,0 +1,13 @@
+//
+//  OCXMPPDelegateHandler.h
+//  iosComm
+//
+//  Created by Kevin Chen on 10/4/12.
+//  Copyright (c) 2012 OpenComm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCXMPPDelegateHandler : NSObject
+
+@end
---
+++ 2012_2013/iOS/iosComm/iosComm/OCXMPPDelegateHandler.m
@@ -0,0 +1,13 @@
+//
+//  OCXMPPDelegateHandler.m
+//  iosComm
+//
+//  Created by Kevin Chen on 10/4/12.
+//  Copyright (c) 2012 OpenComm. All rights reserved.
+//
+
+#import "OCXMPPDelegateHandler.h"
+
+@implementation OCXMPPDelegateHandler
+
+@end
