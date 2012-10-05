a2f983651af0ca90e85aa6b899c3ad19d0568b25
Nora N-Q <ncn27@cornell.edu>
2012-10-05 01:07:13 -0400

    Added Contacts page. Still need button images

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/model/User.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/model/User.java
@@ -1,5 +1,23 @@
 package edu.cornell.opencomm.model;
 
 public class User {
-
+	private String firstName;
+	private String lastName;
+	// private Image picture;
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public String getFullName(){
+		return firstName + ' ' + lastName;
+	}
+	/*
+	public Image getPicture(){
+		return picture;
+	} */
 }
