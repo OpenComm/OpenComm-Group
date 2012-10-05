ad611521681c6620bacbf6720eb318d7b5dfac65
Nora N-Q <ncn27@cornell.edu>
2012-10-05 17:15:34 -0400

    Adjusted Contacts page, added functionality to buttons, started Contact Search page

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/ContactsView.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/ContactsView.java
@@ -1,12 +1,21 @@
 package edu.cornell.opencomm.view;
 
 import edu.cornell.opencomm.R;
+import edu.cornell.opencomm.controller.FontSetter;
 import edu.cornell.opencomm.model.ContactsDbAdapter;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+//import android.graphics.PorterDuff.Mode;
+import android.widget.Toast;
 
 public class ContactsView extends Activity{
 	private ContactsDbAdapter dbHelper;
@@ -14,12 +23,77 @@ public class ContactsView extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contacts_layout);
+		adjustLayoutLookAndFunctionality();
 		dbHelper = new ContactsDbAdapter();
 		dbHelper.open();
 		retrieveAndDsiplayContacts();
 	}
 	
+	/** Adjust the displayed layout and button functionality */
+	public void adjustLayoutLookAndFunctionality(){
+		// Apply Delicious Font
+		FontSetter.applySanSerifFont(ContactsView.this,
+		findViewById(R.layout.contacts_layout));
+		
+		// Ensure the actionbar is present
+		// ActionBar actionBar = getActionBar();
+		// actionBar.show();
+		
+		// Initialize images to act as buttons
+		initializeBackButton();
+		initializeSearchButton();
+		
+	}
+	
+	public void initializeBackButton(){
+		final ImageView backButton = (ImageView) findViewById(R.id.left_arrow_icon);
+		backButton.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+	            switch (arg1.getAction()) {
+	            case MotionEvent.ACTION_DOWN: {
+	            	// TODO Change color of button? Ask Design team
+	                break;
+	            }
+	            case MotionEvent.ACTION_UP:{
+	            	// TODO Start the Dashboard Activity - Dashboard Activity has not been made yet however
+	            	//Intent intent = new Intent(ContactsView.this, DashboardView.class);
+	        		//startActivity(intent);
+	            	
+	            	// For now make a toast
+	            	Context context = getApplicationContext();
+	            	CharSequence text = "Go to Dashboard Page!";
+	            	int duration = Toast.LENGTH_SHORT;
+	            	Toast toast = Toast.makeText(context, text, duration);
+	            	toast.show();
+	            }
+	            }
+	            return true;
+	        }
+		}); 
+	} 
+	
+	public void initializeSearchButton(){
+		final ImageView searchButton = (ImageView) findViewById(R.id.search_button);
+		searchButton.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+	            switch (arg1.getAction()) {
+	            case MotionEvent.ACTION_DOWN: {
+	            	// TODO Change color of button? Ask Design team
+	                break;
+	            }
+	            case MotionEvent.ACTION_UP:{
+	            	Intent intent = new Intent(ContactsView.this, ContactsSearchView.class);
+	        		startActivity(intent);
+	                break;
+	            }
+	            }
+	            return true;
+	        }
+		}); 
+	}
+	
 	/** Retrieve and display this user's contacts */
+	// TODO Needs to be tested and altered
 	public void retrieveAndDsiplayContacts(){
 		// Retrieve all contacts 
 		Cursor cursor = dbHelper.getAllContacts();
@@ -37,4 +111,5 @@ public class ContactsView extends Activity{
 		ListView listView = (ListView) findViewById(R.id.contacts_list); 
 		listView.setAdapter(adapter);
 	}
+
 }
