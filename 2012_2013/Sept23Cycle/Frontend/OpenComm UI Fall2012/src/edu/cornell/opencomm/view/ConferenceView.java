3970a69b75e280b6fafdf73180c09570342f7c1e
Heming Ge <hwg33@cornell.edu>
2012-10-14 17:27:03 -0400

    Empty Conference View

---
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/ConferenceView.java
@@ -0,0 +1,12 @@
+package edu.cornell.opencomm.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import edu.cornell.opencomm.R;
+
+public class ConferenceView extends Activity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conference_layout);
+    }
+}
