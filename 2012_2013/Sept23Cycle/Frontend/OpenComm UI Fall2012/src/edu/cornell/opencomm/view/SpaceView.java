7bfbb33a79a4224f23367b7065b2d40a484fd338
Heming Ge <hwg33@cornell.edu>
2012-10-14 17:35:43 -0400

    Empty Chat and Space View

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/SpaceView.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/SpaceView.java
@@ -1,5 +1,16 @@
 package edu.cornell.opencomm.view;
 
-public class SpaceView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SpaceView extends View{
+
+	public SpaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
