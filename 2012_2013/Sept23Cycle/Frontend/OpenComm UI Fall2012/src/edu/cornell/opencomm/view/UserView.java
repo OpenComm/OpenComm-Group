f5195242d49b178b1ed12a3ab4b78ecc3aaa7ff8
Heming Ge <hwg33@cornell.edu>
2012-10-18 17:42:14 -0400

    Space and User Views

--- 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/UserView.java
+++ 2012_2013/Sept23Cycle/Frontend/OpenComm UI Fall2012/src/edu/cornell/opencomm/view/UserView.java
@@ -3,6 +3,9 @@ package edu.cornell.opencomm.view;
 import edu.cornell.opencomm.model.Space;
 import edu.cornell.opencomm.model.User;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,6 +13,11 @@ public class UserView extends View{
     Context context;
     User person;
     Space space;
+    Bitmap image;
+    Paint paint;
+    int x, y; //position of icon
+    boolean ghost = false;
+    
 	public UserView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
@@ -21,4 +29,46 @@ public class UserView extends View{
 	        this.person = person;
 	        this.space = space;
 	 }
+	 
+	 public UserView(Context context, User person, Bitmap image, int x, int y) {
+	        super(context);
+	        this.context = context;
+	        this.person = person;
+	        this.x = x;
+	        this.y = y;
+	        this.image = image;
+	        this.ghost = true;
+	        setPaint(context);
+	    }
+	 
+	 public User getPerson(){
+		 return person;
+	 }
+	 
+	 public UserView getGhost() {
+	        return new UserView(context,person,image,x,y);
+	 }
+	 
+	 public void setPopupImage(Bitmap oldImage){
+	        /*	BitmapFactory.Options opts = new BitmapFactory.Options();
+			opts.inJustDecodeBounds = true;
+			this.image = BitmapFactory.decodeResource(context.getResources(), imageID); */
+	        int width = (oldImage).getWidth();
+	        int height = (oldImage).getHeight();
+	        int newWidth = 42;
+	        int newHeight = 42;
+	        float scaleWidth = ((float) newWidth)/width;
+	        float scaleHeight = ((float) newHeight) / height;
+	        Matrix matrix = new Matrix();
+	        matrix.postScale(scaleWidth, scaleHeight);
+	        this.image = Bitmap.createBitmap(oldImage, 0, 0, width, height, matrix, true);	
+	 }
+	 
+	 public void setPaint(Context context){
+	        paint = new Paint();
+	        paint.setDither(true);
+	        paint.setColor(0xFF000000);;
+	        paint.setStyle(Paint.Style.STROKE);
+	        paint.setStrokeWidth(1);
+	    }
 }
