d88c54a70f084e38151bd38347216e35c2e581ea
= <qf26@cornell.edu>
2012-10-25 13:10:19 -0400

    added storyboard file

---
+++ 2012_2013/iOS/iosMedia/iosMedia/OPCViewController.m
@@ -0,0 +1,29 @@
+//
+//  OPCViewController.m
+//  iosMedia
+//
+//  Created by Qiming Fang on 10/24/12.
+//  Copyright (c) 2012 OpenComm. All rights reserved.
+//
+
+#import "OPCViewController.h"
+
+@interface OPCViewController ()
+
+@end
+
+@implementation OPCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
