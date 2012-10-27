8bb1ee867228ea21f861ec8bc48c891143e466d1
Heming Ge <hwg33@cornell.edu>
2012-10-27 15:57:21 -0400

    ContactCard

---
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactCardController.java
@@ -0,0 +1,25 @@
+package edu.cornell.opencomm.controller;
+
+import edu.cornell.opencomm.view.ContactCardView;
+
+public class ContactCardController {
+
+	private ContactCardView view;
+	
+	public ContactCardController (ContactCardView v){
+		this.view = v;
+	}
+	
+	public void handleBackButtonClicked(){
+		
+	}
+	public void handleAddButtonClicked(){
+		
+	}
+	public void handleOverflowButtonClicked(){
+		
+	}
+	public void handleBlockButtonClicked(){
+		
+	}
+}
