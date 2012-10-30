f451f36d28f61473164644766c20671972e1d981
Ankit <as2536@cornell.edu>
2012-10-30 16:59:50 -0400

    Refactoring Conference

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/model/ChatSpaceModel.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/model/ChatSpaceModel.java
@@ -1,5 +1,7 @@
 package edu.cornell.opencomm.model;
 
+import java.util.HashMap;
+
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
@@ -7,8 +9,18 @@ public class ChatSpaceModel extends MultiUserChat{
 	
 	//TODO:Is it a good idea to extend MUC?
 	//Ask Risa and Kris
+	//How does MUC get list of occupants
+	/**
+	 * 
+	 */
+	HashMap<String, User> userMap = new HashMap<String, User>();
+	
 	public ChatSpaceModel(Connection arg0, String arg1) {
 		super(arg0, arg1);
+		
+		
+		
+		
 		// TODO Auto-generated constructor stub
 	}
 	
