eff9c59e1c6fbcea5583fcb1a314a7960baa33b4
Brian O'Connor <bjo29@cornell.edu>
2012-10-30 16:04:15 -0400

    Contact Card Controller

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactCardController.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactCardController.java
@@ -1,21 +1,29 @@
 package edu.cornell.opencomm.controller;
 
+import java.util.ArrayList;
+
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 
+import android.content.Intent;
+import android.util.Log;
+
+import edu.cornell.opencomm.Manager.UserManager;
 import edu.cornell.opencomm.model.User;
 import edu.cornell.opencomm.network.NetworkService;
 
 import edu.cornell.opencomm.view.ContactCardView;
+import edu.cornell.opencomm.view.ContactsView;
+import edu.cornell.opencomm.view.DashboardView;
 
 
 public class ContactCardController {
 
 	private static final String TAG = "Controller.ContactCardController";
 
-	private Roster roster;
-	NetworkService xmppService;
-	XMPPConnection xmppConn;
+	private User contact;
 
 	private ContactCardView view;
 	
@@ -24,13 +32,14 @@ public class ContactCardController {
 	}
 	
 	public void handleBackButtonClicked(){
-		
+		Intent intent = new Intent(this.view, ContactsView.class); 
+		this.view.startActivity(intent);
 	}
 	public void handleAddButtonClicked(){
-		this.xmppService = NetworkService.getInstance();
-		this.xmppConn = this.xmppService.getConnection();
-		roster = this.xmppConn.getRoster();
-		Log.v(TAG, "adding " + user + " to contact list");
+		Log.v(TAG, "Adding user " + contact.getUsername() + " to contacts");
+		ArrayList<User> contactList = UserManager.getContactList();
+		contactList.add(contact);
+		UserManager.updateContactList(contactList);
 	}
 	public void handleOverflowButtonClicked(){
 		
