91d8269a2563ee14e5ffb5ed3e7bb8fd7e46f0e9
Brian O'Connor <bjo29@cornell.edu>
2012-10-30 15:21:11 -0400

    Contact Card Controller

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactCardController.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactCardController.java
@@ -1,8 +1,21 @@
 package edu.cornell.opencomm.controller;
 
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+
+import edu.cornell.opencomm.model.User;
+import edu.cornell.opencomm.network.NetworkService;
+
 import edu.cornell.opencomm.view.ContactCardView;
 
+
 public class ContactCardController {
+
+	private static final String TAG = "Controller.ContactCardController";
+
+	private Roster roster;
+	NetworkService xmppService;
+	XMPPConnection xmppConn;
 
 	private ContactCardView view;
 	
@@ -14,7 +27,10 @@ public class ContactCardController {
 		
 	}
 	public void handleAddButtonClicked(){
-		
+		this.xmppService = NetworkService.getInstance();
+		this.xmppConn = this.xmppService.getConnection();
+		roster = this.xmppConn.getRoster();
+		Log.v(TAG, "adding " + user + " to contact list");
 	}
 	public void handleOverflowButtonClicked(){
 		
