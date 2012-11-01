01e05bb8b053b3e303e58baf18b8f54e3b7477ee
Alin Barsan <alinbarsan@comcast.net>
2012-10-31 23:39:43 -0400

    ConferenceCommunicator class updated/improved

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/packet/ConferenceCommunicator.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/packet/ConferenceCommunicator.java
@@ -5,6 +5,7 @@ import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.filter.PacketTypeFilter;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Packet;
 
 import android.util.Log;
@@ -15,84 +16,105 @@ import edu.cornell.opencomm.model.Conference;
 public class ConferenceCommunicator {
 
 	public static final String LOG_TAG = "Network.ConferenceCommunicator";
-	public static final boolean D = true;
 	private PacketCollector collector;
 	private PacketListener listener;
 	private XMPPConnection xmppConn;
 	private static final String PULL_SUBJECT = "pullConference";
-	private static final String SUBJECT_PROPERTY_NAME = "subject";
+	private static final String PULL_CONFIRMATION_SUBJECT = "ConferenceInfo";
+	// subject in packets from database in response to pull request
+	private static final String PUSH_CONFIRMATION_SUBJECT = "ConferencePushResult";
+	// subject in packets from database in response to pull request
+	private static final String PUSH_SUCCESS = "SUCCESS";
+	private static final String PUSH_FAILURE = "FAILURE";
+	private static final String PULL_FAILURE = null;
+
+	//
 
 	public ConferenceCommunicator(XMPPConnection xmppConn) {
 		this.xmppConn = xmppConn;
-		// TODO figure out if connection is acceptable, since Chat sends
-		// messages, not packets, and see if we can just have a connection to
-		// database so we are not broadcasting packets to everybody
-		PacketFilter filter = new PacketTypeFilter(ConferencePacket.class);
+		PacketFilter filter = new PacketTypeFilter(Message.class);
 		collector = xmppConn.createPacketCollector(filter);
 		// collector will allow us to build a que of packets and poll later
 		// instead of instantly processing all of the packet upon arrival
 		listener = new PacketListener() {
 			public void processPacket(Packet arg0) {
-				Log.v(LOG_TAG,
-						"packet received with subject:"
-								+ arg0.getProperty(SUBJECT_PROPERTY_NAME));
+				if (arg0 instanceof Message) {
+					// listens for message packets only, since that is what the
+					// database sends back, this is just for debug confirmation
+					// of packet listening/receiving
+					Message arg0Message = (Message) arg0;
+					Log.v(LOG_TAG, "packet received with subject:"
+							+ arg0Message.getSubject());
+				}
+
 			}
 		};
 	}
 
 	/** Sends conference info to the database. */
-	public void pushConference(Conference conference) {
+	public boolean pushConference(Conference conference) {
 		ConferencePacket packet = conference.toPacket();
 		packet.setFrom(xmppConn.getUser());
 		xmppConn.sendPacket(packet);
 		Log.v(LOG_TAG, "push message sent");
+		Message received = null;
+		while (received == null
+				|| !received.getSubject().equals(PUSH_CONFIRMATION_SUBJECT)) {
+			// poll next packet in que until one with push confirmation is found
+			received = (Message) collector.nextResult();
+			// cast is safe since all collected packets are filtered for message
+		}
+		if (received.getBody().equals(PUSH_SUCCESS)) {
+			Log.v(LOG_TAG, "push successful");
+			return true;
+		} else if (received.getBody().equals(PUSH_FAILURE)) {
+			Log.v(LOG_TAG, "push failed");
+			return false;
+		} else {
+			Log.v(LOG_TAG,
+					"push may or may not have failed, body of push confirmation packet was of the wrong format");
+			return false;
+		}
 	}
 
 	/**
-	 * Sends a message to the database asking for conference data for the
-	 * conference with the given roomID.
+	 * Sends a packet to database requesting info on a conference with certain
+	 * roomID
+	 * 
+	 * @param roomID
+	 *            - roomID of conference to pull
+	 * @return String - returns null if conference not found in database, and if
+	 *         conference is found returns data in format: roomID + "//" +
+	 *         roomname + "//" invitername + "//" starttime + "//" + endtime +
+	 *         "//" recurrence + "//" description + "//" + participant1'sName +
+	 *         "//" + participant2'sName... + "//" + participantN'sName + "\n"
+	 *         (Note: starttime and endtime are in format:
+	 *         "YYYY-MM-DD HH:MM:SS")
 	 */
-	public Conference pullConference(int roomID) {
+	public String pullConference(int roomID) {
+		// note: return type is string. Cannot return conference, since database
+		// does not store sufficient information to call conference constructor
+		// (since constructor requires user
 		ConferencePacket packet = new ConferencePacket(roomID, PULL_SUBJECT);
 		packet.setFrom(xmppConn.getUser());
 		xmppConn.sendPacket(packet);
 		Log.v(LOG_TAG, "pull message sent");
-		ConferencePacket polled = (ConferencePacket) collector.pollResult();
-		// grabs the next packet in the que, if there is one.
-		if (polled != null) {
-			String subject = (String) polled.getProperty(SUBJECT_PROPERTY_NAME);
-			// cast is safe since ConferencePacket only stores "subject" as a
-			// string
-			if (!subject.equals(PULL_SUBJECT)) {
-				// packet is not the one we sent, must be from database
-				// TODO - implement check for validity of returned packet?
-				// i.e:
-				/*
-				 * if(subject.equals(RETURN_SUBJECT)){ //return packet } else{
-				 * //return null }
-				 */
-				Log.v(LOG_TAG, "conference successfully pulled from database");
-				return polled.toObject();
-			} else {
-				// if we caught the packet we sent, it has been removed from the
-				// collector que by the pollResult() call, so call nextResult()
-				// which will block method until we get the next packet, or
-				// return it if it is already in the collector's que
-				Log.v(LOG_TAG,
-						"removed pull request from collector que, waiting for next packet");
-				ConferencePacket resultPacket = (ConferencePacket) collector
-						.nextResult();
-				// NOTE: can call nextResult(long timeout) (in milliseconds) to
-				// return null upon timeout
-				Log.v(LOG_TAG,
-						"packet found. conference successfully pulled from database");
-				return resultPacket.toObject();
-			}
+		Message received = null;
+		while (received == null
+				|| !received.getSubject().equals(PULL_CONFIRMATION_SUBJECT)) {
+			// poll next packet in que until one with pull confirmation is found
+			received = (Message) collector.nextResult();
+			// cast is safe since all collected packets are filtered for message
 		}
-		// no packets were collected - return null to satisfy method return type
-		// this should not occur - but if method consistently returns null this
-		// could be the cause, if our collector is not be functioning properly
-		return null;
+		if (received.getBody() == null || received.getBody().equals("")) {
+			Log.v(LOG_TAG, "pull failed");
+			return PULL_FAILURE;
+		} else {
+			Log.v(LOG_TAG,
+					"pull successful.  returned info: " + received.getBody());
+			return received.getBody();
+		}
+
 	}
 
 }
