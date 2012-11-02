34e6762fa105d769ca42f4994ef7cabda9e10d0a
Sauhard Bindal <sb673@cornell.edu>
2012-11-02 14:22:02 -0400

    Contact cell added
    Added a design for contact cell..

---
+++ 2012_2013/iOS/iosComm/iosComm/OCContactCell.h
@@ -0,0 +1,17 @@
+//
+//  OCContactCell.h
+//  iosComm
+//
+//  Created by Sauhard Bindal on 11/2/12.
+//  Copyright (c) 2012 OpenComm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCContactCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *ContactNameLabel;
+
+@property (nonatomic, weak) IBOutlet UIImageView *thumbnailImageView;
+
+@end
---
+++ 2012_2013/iOS/iosComm/iosComm/OCContactCell.m
@@ -0,0 +1,29 @@
+//
+//  OCContactCell.m
+//  iosComm
+//
+//  Created by Sauhard Bindal on 11/2/12.
+//  Copyright (c) 2012 OpenComm. All rights reserved.
+//
+
+#import "OCContactCell.h"
+
+@implementation OCContactCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
