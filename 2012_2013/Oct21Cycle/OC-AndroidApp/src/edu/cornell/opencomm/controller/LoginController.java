f94714b87a38eecf3e35f1f80fb358eeaaa280cd
Risa Naka <rn96@cs.cornell.edu>
2012-11-03 16:45:30 -0400

    LoginView reformatted

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/LoginController.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/LoginController.java
@@ -17,6 +17,21 @@ import edu.cornell.opencomm.view.PopupView;
 import edu.cornell.opencomm.view.ResetPasswordView;
 import edu.cornell.opencomm.view.SignupView;
 
+/**
+ * View for login screen.
+ * Functionality (handled by LoginController)
+ * <ul>
+ * <li>When the user has forgotten a password, it launches ResetPasswordView.</li>
+ * <li>When the user wants to sign up, it launches SignupView.</li>
+ * <li>When the user attempts to log in, checks for valid inputs, and then 
+ * attempts to log in, and if successful, launches DashboardView</li>
+ * </ul>
+ *
+ * Issues [TODO]
+ * - For any other issues search for string "TODO"
+ *
+ * @author Heming Ge[frontend], Ankit Singh[frontend]
+ * */
 public class LoginController {
 
 	@SuppressWarnings("unused")
