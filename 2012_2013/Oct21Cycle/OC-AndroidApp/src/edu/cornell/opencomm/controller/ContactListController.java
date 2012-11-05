e30d9a9c719ce938c3bc90e49456ce16c20fa9a6
Spandana Govindgari <sgovindgari@gmail.com>
2012-11-05 04:30:10 -0500

    contact search complete implementation

--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactListController.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/controller/ContactListController.java
@@ -9,20 +9,19 @@ import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.XMPPConnection;
 
-import edu.cornell.opencomm.R;
-import edu.cornell.opencomm.Manager.UserManager;
-import edu.cornell.opencomm.model.User;
-import edu.cornell.opencomm.network.NetworkService;
-import edu.cornell.opencomm.view.ContactCardView;
-import edu.cornell.opencomm.view.ContactsSearchView;
-import edu.cornell.opencomm.view.ContactsView;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import edu.cornell.opencomm.R;
+import edu.cornell.opencomm.Manager.UserManager;
+import edu.cornell.opencomm.model.User;
+import edu.cornell.opencomm.network.NetworkService;
+import edu.cornell.opencomm.view.ContactCardView;
+import edu.cornell.opencomm.view.ContactsView;
 
 public class ContactListController {
 
@@ -71,10 +70,18 @@ public class ContactListController {
 		//TODO: Needs to find the contact and open that contact page
 	}
 
+	public void contactSearchContactClicked(View v){
+		Intent i = new Intent(this.view, ContactCardView.class);
+		Button user = (Button) v.findViewById(R.id.contact_search_name);
+		i.putExtra("Contact", user.getText().toString());
+		this.view.startActivity(i);
+		//TODO: Needs to find the contact and open that contact page
+	}
+
 	public ReturnState updateContacts() {
 		roster = this.xmppConn.getRoster();
 		AsyncTask<Void, Void, ReturnState> populate = new PopulateContactsTask()
-				.execute();
+		.execute();
 		try {
 			return (ReturnState) populate.get();
 		} catch (InterruptedException e) {
@@ -86,7 +93,7 @@ public class ContactListController {
 	}
 
 	private class PopulateContactsTask extends
-			AsyncTask<Void, Void, ReturnState> {
+	AsyncTask<Void, Void, ReturnState> {
 		protected void onProgressUpdate(Void... values) {
 			super.onProgressUpdate(values);
 		}
@@ -134,7 +141,8 @@ public class ContactListController {
 	}
 
 	/**
-	 * 
+	 * ISSUES - Not sure if this algorithm is actually consistent
+	 * Gave different results each time- weird O.o
 	 * @param charQuery
 	 *            - A sequence of characters representing a user's query for his
 	 *            contacts
@@ -142,82 +150,88 @@ public class ContactListController {
 	 *         query. Returns null if none found or there are no contacts
 	 */
 	public ArrayList<User> filterContacts(CharSequence charQuery) {
-		ArrayList<User> users = UserManager.getContactList();
-		String query = charQuery.toString();
-		String queryStartsWith = query.substring(0, 1);
-		if (D) {
-			Log.v(LOG_TAG, "query results in string: " + query);
-			Log.v(LOG_TAG, "retreived contact list of length " + users.size());
-		}
-		if (users == null || users.size() == 0) {
-			return null;
-		}
-		ArrayList<User> filtered = new ArrayList<User>();
-		int start = 0;
-		int end = users.size();
-		int prevMiddle = -1;
-		int middle = end / 2;
-		while (middle != prevMiddle) {
-			if (users.get(middle).getUsername().substring(0,1).equalsIgnoreCase(queryStartsWith)) {
-				// finds block of users that start with the first letter
-				// finds the index of the first user in the block with that
-				// first letter
-				Log.v(LOG_TAG,
-						"found user(s) with a username that starts with the first character of the query");
-				int blockFirstIndex = middle;
-				for (int i = middle; i > 0; i--) {
-					if (users.get(i - 1).getUsername()
-							.substring(0,1).equalsIgnoreCase(queryStartsWith)) {
-						blockFirstIndex = i - 1;
-					} else {
-						break;
-						// do not continue to iterate once the first letter does
-						// not match
-					}
-				}
-
-				// iterates through block to find users w/ names matching query
-				for (int i = blockFirstIndex; i < users.size(); i++) {
-					User userI = users.get(i);
-					if (!userI.getUsername().substring(0,1).equalsIgnoreCase(queryStartsWith)) {
-						// do not continue to iterate once the first letter does
-						// not match
-						break;
-					}
-					if (userI.getUsername().substring(0,query.length()).equalsIgnoreCase(query)) {
-						filtered.add(users.get(i));
-					}
-				}
-				if (filtered.size() == 0) {
-					// makes sure that null is returned instead of empty
-					// arraylist if no matches to query were found
-					Log.v(LOG_TAG, "found no user(s) with a username that starts with the query");
-					return null;
-				}
-				Log.v(LOG_TAG, "found user(s) with a username that starts with the query");
-				return filtered;
-			} else {
-				prevMiddle = middle;
-				if (users.get(middle).getUsername().substring(0, 1)
-						.compareTo(queryStartsWith) < 0) {
-					// compares the first letter of the username with the first
-					// letter of the query, if the letter of the username is
-					// before the letter of the query, we should look in the
-					// upper half of the arraylist (we can set our firstIndex to
-					// our value of middle)
-					start = middle;
-				} else {
-					// safe to do else since == case is handled by very first
-					// check in the greater loop
-					end = middle;
-				}
-				middle = (start + end) / 2;
+		try{
+			ArrayList<User> users = UserManager.getContactList();
+			String query = charQuery.toString();
+			String queryStartsWith = query.substring(0, 1);
+			if (D) {
+				Log.v(LOG_TAG, "query results in string: " + query);
+				Log.v(LOG_TAG, "retreived contact list of length " + users.size());
 			}
+			if (users == null || users.size() == 0) {
+				return null;
+			}
+			ArrayList<User> filtered = new ArrayList<User>();
+			int start = 0;
+			int end = users.size();
+			int prevMiddle = -1;
+			int middle = end / 2;
+			while (middle != prevMiddle) {
+				if (users.get(middle).getUsername().substring(0,1).equalsIgnoreCase(queryStartsWith)) {
+					// finds block of users that start with the first letter
+					// finds the index of the first user in the block with that
+					// first letter
+					Log.v(LOG_TAG,
+							"found user(s) with a username that starts with the first character of the query");
+					int blockFirstIndex = middle;
+					for (int i = middle; i > 0; i--) {
+						if (users.get(i - 1).getUsername()
+								.substring(0,1).equalsIgnoreCase(queryStartsWith)) {
+							blockFirstIndex = i - 1;
+						} else {
+							break;
+							// do not continue to iterate once the first letter does
+							// not match
+						}
+					}
+
+					// iterates through block to find users w/ names matching query
+					for (int i = blockFirstIndex; i < users.size(); i++) {
+						User userI = users.get(i);
+						if (!userI.getUsername().substring(0,1).equalsIgnoreCase(queryStartsWith)) {
+							// do not continue to iterate once the first letter does
+							// not match
+							break;
+						}
+						if (userI.getUsername().substring(0,query.length()).equalsIgnoreCase(query)) {
+							filtered.add(users.get(i));
+						}
+					}
+					if (filtered.size() == 0) {
+						// makes sure that null is returned instead of empty
+						// arraylist if no matches to query were found
+						Log.v(LOG_TAG, "found no user(s) with a username that starts with the query");
+						return null;
+					}
+					Log.v(LOG_TAG, "found user(s) with a username that starts with the query");
+					return filtered;
+				} else {
+					prevMiddle = middle;
+					if (users.get(middle).getUsername().substring(0, 1)
+							.compareTo(queryStartsWith) < 0) {
+						// compares the first letter of the username with the first
+						// letter of the query, if the letter of the username is
+						// before the letter of the query, we should look in the
+						// upper half of the arraylist (we can set our firstIndex to
+						// our value of middle)
+						start = middle;
+					} else {
+						// safe to do else since == case is handled by very first
+						// check in the greater loop
+						end = middle;
+					}
+					middle = (start + end) / 2;
+				}
+			}
+			// if middle = PrevMiddle, then we have reached a point where the letter
+			// is not being found and there are no more divisions to make, nothing
+			// matches query, so return null
+			Log.v(LOG_TAG, "found no user(s) with a username that starts with the first character of the query");
+			return null;
+		}catch(StringIndexOutOfBoundsException e){
+			//Do nothing
+			//TODO- actually has to wait for the user and not throw a toast
+			return null; 
 		}
-		// if middle = PrevMiddle, then we have reached a point where the letter
-		// is not being found and there are no more divisions to make, nothing
-		// matches query, so return null
-		Log.v(LOG_TAG, "found no user(s) with a username that starts with the first character of the query");
-		return null;
 	}
 }
--- 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/view/ContactsSearchView.java
+++ 2012_2013/Oct21Cycle/OC-AndroidApp/src/edu/cornell/opencomm/view/ContactsSearchView.java
@@ -1,38 +1,133 @@
 package edu.cornell.opencomm.view;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 import edu.cornell.opencomm.R;
 import edu.cornell.opencomm.controller.ContactListController;
 import edu.cornell.opencomm.controller.FontSetter;
+import edu.cornell.opencomm.model.ContactSearchAdapter;
+import edu.cornell.opencomm.model.User;
 
 public class ContactsSearchView extends Activity {
-	
-	
+
+
 	private ContactListController controller; 
-	
+	private ListView results; 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 * For debugging purposes
+	 *
+	 */
+	private final boolean D = true; 
+	private final String TAG= ContactsSearchView.class.getSimpleName(); 
+
+	private EditText query; 
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.contact_search_layout);
+		this.query = (EditText)findViewById(R.id.search_contact_bar); 
+		this.results = (ListView)findViewById(R.id.contact_search_list); 
 		FontSetter.applySanSerifFont(this, findViewById(R.layout.contact_search_layout));
 		FontSetter.applySanSerifFont(this, findViewById(R.layout.contact_entry_layout));
 		controller = new ContactListController(this);
+		textEntered();
 	}
-	
+
 	//TODO
 	//1.Method for getting the current text and searching database - cursor adapter
-	
+
 	//when a contact is clicked
 	public void goToContact(View v){
-		this.controller.contactClicked(v);
+		this.controller.contactSearchContactClicked(v);
 	}
-	
+
 	public void backButtonPressed(View v){
 		this.controller.handleBackButtonClicked();
 	}
-	
+
 	public void blockButtonPressed(View v){
 		this.controller.handleBlockButtonClicked();
 	}
+
+	//TODO/ISSUES -
+	//1. The cursor goes back to start 
+	//2. This only happens when the editText box is clicked - must make it happen when its not clicked as well
+	//3. Basically when any changes have been made to the editText box. 
+
+	//As the user enters text- this method gets the text and sends
+	//queries to the database to retrieve contacts
+	public void textEntered(){
+		this.query.setText("");
+		if (D){Log.v(TAG,query.getText().toString());}
+		query.addTextChangedListener(new TextWatcher(){
+			//these methods can cause StackOverflow Error- must be very careful!
+			public void afterTextChanged(Editable arg0) {
+				if (D){Log.v(TAG, "afterTextChanged"+ arg0.toString());}
+				String changedText = arg0.toString(); 
+				query.removeTextChangedListener(this);
+				query.setText(changedText);			
+				searchQuery(changedText);
+				query.addTextChangedListener(this);
+				// TODO Auto-generated method stub
+
+			}
+
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+			}
+
+		}); 
+	}
+
+	public void searchQuery(CharSequence query){
+		this.controller.updateContacts(); 
+		if (query != null || query != ""){
+			ArrayList<User> contacts = this.controller.filterContacts(query);
+			if (contacts != null){
+				ContactSearchAdapter adapter = new ContactSearchAdapter(this, R.layout.contact_search_entry_layout, contacts);
+				results.setAdapter(adapter);
+				results.setVisibility(View.VISIBLE);
+				results.setOnItemClickListener(new OnItemClickListener(){
+
+					public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+							long arg3) {
+						controller.contactSearchContactClicked(arg1);			
+					}			
+				});
+			}
+			//no contacts that match the query 
+			//TODO - ask the design team what to do in this case?
+			//For now throwing a toast
+			else{
+				results.setVisibility(View.INVISIBLE);
+				CharSequence text = "No contacts found matching the query";
+				int duration = Toast.LENGTH_SHORT;
+				Toast send = Toast.makeText(getApplicationContext(),text,duration);
+				send.show();
+			}
+		}
+	}
+
 }
+
+
