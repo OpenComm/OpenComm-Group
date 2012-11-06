9e25a5a4de9c85a3004ff72e47fa7b1f204c4811
Ankit Singh <as2536@cornell.edu>
2012-11-06 15:51:39 -0500

    user Location map

--- 2012_2013/Nov4Cycle/OC-AndroidApp/src/edu/cornell/opencomm/model/ChatSpaceModel.java
+++ 2012_2013/Nov4Cycle/OC-AndroidApp/src/edu/cornell/opencomm/model/ChatSpaceModel.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smackx.muc.MultiUserChat;
 
+import android.graphics.Point;
+
 public class ChatSpaceModel extends MultiUserChat{
 	
 	//TODO:Is it a good idea to extend MUC?
@@ -14,7 +16,7 @@ public class ChatSpaceModel extends MultiUserChat{
 	 * 
 	 */
 	HashMap<String, User> userMap = new HashMap<String, User>();
-	
+	HashMap<User, Point> userLocationMap = new HashMap<User, Point>();
 	public ChatSpaceModel(Connection arg0, String arg1) {
 		super(arg0, arg1);
 		
@@ -22,6 +24,12 @@ public class ChatSpaceModel extends MultiUserChat{
 		
 		
 		// TODO Auto-generated constructor stub
+	}
+	public void updateUserLocation(User u, Point location){
+		
+	}
+	public void getUserLocation(User u){
+		
 	}
 	
 
