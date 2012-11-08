cc79262388870c9601df48d6e5bfe5426a418f47
Nora N-Q <ncn27@cornell.edu>
2012-11-08 10:57:07 -0500

    Can swipe between mainchat and sidechats

---
+++ 2012_2013/Nov4Cycle/OC-AndroidApp/src/edu/cornell/opencomm/view/ConferencePageAdapter.java
@@ -0,0 +1,61 @@
+package edu.cornell.opencomm.view;
+
+import edu.cornell.opencomm.R;
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class ConferencePageAdapter extends PagerAdapter {
+	ConferenceView conferenceView;
+	
+	public ConferencePageAdapter(ConferenceView conferenceView){
+		this.conferenceView = conferenceView;
+	}
+	
+    public int getCount() {
+        return 3;
+    }
+    public Object instantiateItem(View collection, int position) {
+        LayoutInflater inflater = (LayoutInflater) collection.getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        int resId = 0;
+        
+        // TODO Spandana ... Decide which layout to use (can use multiple if you want)
+        resId = R.layout.conference_room_layout;
+        
+        View view = inflater.inflate(resId, null);
+        ((ViewPager) collection).addView(view, 0);
+        
+        // TODO if you want to customize information
+        TextView title = (TextView) view.findViewById(R.id.text);
+        switch (position) {
+        case 0:
+        	title.setText("Left Sidechat");
+        	break;
+        case 1:
+        	title.setText("Main Chat");
+        	break;
+        case 2:
+        	title.setText("Right Sidechat");
+        	break;
+        }
+        conferenceView.switchChatSpaceModel(); // TODO Ankit ... should this method be called here?
+        return view;
+    }
+    @Override
+    public void destroyItem(View arg0, int arg1, Object arg2) {
+        ((ViewPager) arg0).removeView((View) arg2);
+    }
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == ((View) arg1);
+    }
+    @Override
+    public Parcelable saveState() {
+        return null;
+    }
+}
--- 2012_2013/Nov4Cycle/OC-AndroidApp/src/edu/cornell/opencomm/view/ConferenceView.java
+++ 2012_2013/Nov4Cycle/OC-AndroidApp/src/edu/cornell/opencomm/view/ConferenceView.java
@@ -5,7 +5,10 @@ package edu.cornell.opencomm.view;
 import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.LinearLayout;
+import edu.cornell.opencomm.R;
 import edu.cornell.opencomm.controller.ConferenceController;
 import edu.cornell.opencomm.model.ConferenceDataModel;
 //TODO: Remove this
@@ -25,38 +28,28 @@ public final class ConferenceView extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+		// TODO temporary layout with real one (conference_layout) once xml errors are fixed
+		setContentView(R.layout.conference_layout_temporary);
 		init();
 	}
 	private void init(){
 		//1. Bind to the conference model
 		//2. instantiate the conference controller
 		//3. register to listners
-		//3.1 Nora : Register the onSwipe listners
 		
+		//3.1 Nora : Register the onSwipe listeners
+		ConferencePageAdapter adapter = new ConferencePageAdapter(this);
+		ViewPager myPager = (ViewPager) findViewById(R.id.threepanelpager);
+		myPager.setAdapter(adapter);
+		myPager.setCurrentItem(1);
 	}
+	
 	/**
 	 * To be invoked when occupant(s) location is changed in the 
 	 * active chat space
 	 */
 	void refreshChatSpaceView(){
-		
-	}
-	/**When the user swipes left
-	 * @param v
-	 */
-	public void onLeftSwipe(View v){
-		
-	}
-	/**When the user swipes right
-	 * @param v
-	 */
-	public void onRightSwipe(View v){
-		
-	}
-	public void onTap(){
 		
 	}
 	
@@ -68,6 +61,25 @@ public final class ConferenceView extends Activity {
 	 */
 	void switchChatSpaceModel(){
 		
+	}
+	
+	public void onTap(){
+		
+	}
+	/** When the user swipes right
+	 * Display the left sidechat
+	 */
+	public void onLeftToRightSwipe(View v) {
+		/* TODO Ankit I don't think I need this... I can just call
+		 * switchChatSpaceModel from ConferencePageAdapter... unless you 
+		 * want to split it up somehow.
+		 */
+	}
+	/** When the user swipes left 
+	 * Display the right sidechat
+	 */
+	public void onRightToLeftSwipe(View v) {
+		// TODO Ankit... same comment as onLeftToRightSwipe
 	}
 	
 }
