410273fd4f9c145ebe28838a65d1c651c7b7349a
Kevin Chen <chen.kevin.f@gmail.com>
2012-11-18 18:33:57 -0500

    finished merging neeleshs stuff but its incomplete. i could not transfer over the constraints for each field, so neelesh will most likely need to put those in again... but everything action wise and transitional wise between the screens is good as far as i can see. tested on the phone

---
+++ 2012_2013/iOS/iosComm/iosComm/OCForgotPasswordViewController.h
@@ -0,0 +1,20 @@
+//
+//  OCForgotPasswordViewController.h
+//  iOSCommUI
+//
+//  Created by Neelesh Bagga on 11/15/12.
+//  Copyright (c) 2012 Sauhard Bindal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCForgotPasswordViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *enteremailField;
+- (IBAction)resetButton:(id)sender;
+- (IBAction)backButtonPressed:(id)sender;
+
+
+@property (strong, nonatomic) IBOutlet UIView *firstView;
+- (IBAction)textfieldReturned:(id)sender;
+
+@end
