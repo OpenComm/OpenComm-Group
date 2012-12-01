c0f0f4b93638f1f24bb9333e8afecc97d0c66eb0
Kevin Chen <chen.kevin.f@gmail.com>
2012-12-01 12:43:24 -0500

    working on the backend room stuff now

--- 2012_2013/iOS/iosComm/iosComm/OCMUCExampleViewController.h
+++ 2012_2013/iOS/iosComm/iosComm/OCMUCExampleViewController.h
@@ -11,4 +11,16 @@
 
 @interface OCMUCExampleViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *participantsLabel;
+@property (strong, nonatomic) IBOutlet UILabel *fifthMessageLabel;
+@property (strong, nonatomic) IBOutlet UIView *fourthMessageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *thirdMessageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *secondMessageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *firstMessageLabel;
+@property (strong, nonatomic) IBOutlet UITextView *textMessage;
+- (IBAction)sendMessage:(id)sender;
+
+//TODO make this a delegateHandler for MUC chat receivals... for now
+
+
 @end
--- 2012_2013/iOS/iosComm/iosComm/OCMUCExampleViewController.m
+++ 2012_2013/iOS/iosComm/iosComm/OCMUCExampleViewController.m
@@ -19,6 +19,11 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        // Core storage
+        //XMPPRoomCoreDataStorage *roomDataStorage = [[XMPPRoomCoreDataStorage alloc] init];
+        //roomDataStorage
+        
+        [delegateHandler myXMPPStream];
     }
     return self;
 }
@@ -28,13 +33,59 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    NSLog(@"I get in here");
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setParticipantsLabel:nil];
+    [self setFifthMessageLabel:nil];
+    [self setFourthMessageLabel:nil];
+    [self setThirdMessageLabel:nil];
+    [self setSecondMessageLabel:nil];
+    [self setFirstMessageLabel:nil];
+    [self setTextMessage:nil];
+    [super viewDidUnload];
+}
+
+- (IBAction)sendMessage:(id)sender {
+    
+}
+
+- (void)xmppRoomDidCreate:(XMPPRoom *)sender
+{
+	NSLog(@"did create room");
+}
+
+- (void)xmppRoomDidJoin:(XMPPRoom *)sender
+{
+	NSLog(@"did join room");
+}
+
+- (void)xmppRoomDidLeave:(XMPPRoom *)sender
+{
+    NSLog(@"did leave room");
+}
+
+- (void)xmppRoom:(XMPPRoom *)sender occupantDidJoin:(XMPPJID *)occupantJID
+{
+	NSLog(@"occupant did join: %@", [occupantJID full]);
+}
+
+- (void)xmppRoom:(XMPPRoom *)sender occupantDidLeave:(XMPPJID *)occupantJID
+{
+	NSLog(@"occupant did join: %@", [occupantJID full]);
+}
+
+- (void)xmppRoom:(XMPPRoom *)sender didReceiveMessage:(XMPPMessage *)message fromOccupant:(XMPPJID *)occupantJID
+{
+	NSLog(@"did receive msg from: %@", [occupantJID full]);
 }
 
 @end
