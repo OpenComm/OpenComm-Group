6913a57373b11942e920a5d5e7851c146bb200f2
Sauhard Bindal <sb673@cornell.edu>
2012-12-02 03:59:14 -0500

    Pop up when attempting to call a conference

--- 2012_2013/iOS/iosComm/iosComm/OCContactsViewController.m
+++ 2012_2013/iOS/iosComm/iosComm/OCContactsViewController.m
@@ -253,176 +253,10 @@ shouldReloadTableForSearchString:(NSString *)searchString
  
  XMPPUserCoreDataStorageObject *user = [[self fetchedResultsController] objectAtIndexPath:indexPath];
  
- cell.textLabel.text = user.displayName;
+     cell.textLabel.text = user.displayName;
  
  return cell;
  }
- // Sweet finish Sauhard todo
-
-/* Sweet COmmented Out
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"ContactCell";
-    OCContactCell *cell = (OCContactCell *) [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    //UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    if (cell == nil) {
-        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"ContactCell" owner:self options:nil];
-        cell = [nib objectAtIndex:0];
-    }
-    
-    if (tableView == self.searchDisplayController.searchResultsTableView) {
-        cell.contactNameLabel.text = [searchResults objectAtIndex:indexPath.row];
-    }
-    else {
-        NSString *book = [[sections valueForKey:[[[sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
-        cell.contactNameLabel.text = book;
-    }
-    cell.thumbnailImageView.image = [UIImage imageNamed:@"default.png"];
-    // Configure the cell...
-    return cell;
-}
-
-
-
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
-{
-    if (tableView == self.searchDisplayController.searchResultsTableView) {
-        return NULL;
-    }
-    else{
-        return [[[sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:section];
-    }
-}
-
-- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView
-{
-    if (tableView == self.searchDisplayController.searchResultsTableView) {
-        return NULL;
-    }
-    else {
-        NSMutableArray *sectionedArray = [[NSMutableArray alloc]init];
-        for(char c ='A' ; c <= 'Z' ;  c++)
-        {
-            [sectionedArray addObject:[NSString stringWithFormat:@"%c",c]];
-        }
-        return [[NSArray arrayWithObject:UITableViewIndexSearch] arrayByAddingObjectsFromArray:sectionedArray];
-    }
-}
-
-
-- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
-{
-    if (title == UITableViewIndexSearch) {
-        CGRect searchBarFrame = self.searchDisplayController.searchBar.frame;
-        [tableView scrollRectToVisible:searchBarFrame animated:NO];
-        return -1;
-    }
-    else{
-        NSInteger count = 0;
-        for(NSString *character in indexArray)
-        {
-            if([character isEqualToString:title])
-            {
-                currentSection = count;
-                return count;
-            }
-            count ++;
-        }
-        return currentSection;
-    }
-}
-
-- (void) setUpContactsList
-{
-    // Need to refactor code
-    BOOL found;
-    
-    // Loop through the books and create our keys
-    for (NSString *book in contactsArray)
-    {
-        NSString *c = [book substringToIndex:1];
-        // NSLog(@"c: %@", c);
-        found = NO;
-        
-        for (NSString *str in [sections allKeys])
-        {
-            if ([str isEqualToString:c])
-            {
-                found = YES;
-            }
-        }
-        
-        if (!found)
-        {
-            // NSLog(@"!found");
-            [sections setValue:[[NSMutableArray alloc] init] forKey:c];
-            //NSLog(@"Sections = %@", sections);
-        }
-    }
-    // NSLog(@"Sections: %@", sections);
-    
-    
-    // Loop again and sort the books into their respective keys
-    for (NSString *book in contactsArray)
-    {
-        [[sections objectForKey:[book substringToIndex:1]] addObject:book];
-    }
-    // NSLog(@"Sections2: %@", sections);
-    
-    
-    // Sort each section array
-    for (NSString *key in [sections allKeys])
-    {
-        NSArray *temp =[sections objectForKey:key];
-        temp = [temp sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-        [sections setValue:temp forKey:key];
-    }
-    
-    // NSLog(@"Sections3: %@", sections);
-}
-*/
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 // SWEET Commented out
 #pragma mark - Table view delegate
@@ -444,6 +278,19 @@ shouldReloadTableForSearchString:(NSString *)searchString
     /*Fetch cell*/
     
     XMPPUserCoreDataStorageObject *user = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+    NSRange conferences = [user.jid.domain rangeOfString:@"conference.cuopencomm"];
+    if (conferences.location != NSNotFound) {
+        UIAlertView *errorAlert = [
+                                   [UIAlertView alloc]
+                                   initWithTitle:@"Calling Conference Error"
+                                   message: @"Silly, you can't call a conference yet!"
+                                   delegate:nil
+                                   cancelButtonTitle: @"OK"
+                                   otherButtonTitles: nil
+                                   ];
+        [errorAlert show];
+        return ;
+    }
     XMPPJID *JID = [XMPPJID jidWithUser: [[user.jidStr componentsSeparatedByString:@"@"] objectAtIndex:0] domain: [[delegateHandler getDefaults] DEFAULT_DOMAIN] resource:[[delegateHandler getDefaults] DEFAULT_RESOURCE]];
     
     //alloc and init a new OCJingleImpl object (destroy the old one if it is not nil)
