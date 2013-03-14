9d383bdbf496c3eacce35c38b8d811471696206d
Antoine Chkaiban <antoine.chkaiban@student.ecp.fr>
2013-03-14 11:30:34 -0400

    Uploaded Working Demo

---
+++ Spring2013/Training Projects/WorkingDemo/src/com/example/view/ChatHandler.java
@@ -0,0 +1,310 @@
+package com.example.view;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.SmackConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.provider.PrivacyProvider;
+import org.jivesoftware.smack.provider.ProviderManager;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.GroupChatInvitation;
+import org.jivesoftware.smackx.PrivateDataManager;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.muc.Occupant;
+import org.jivesoftware.smackx.packet.ChatStateExtension;
+import org.jivesoftware.smackx.packet.LastActivity;
+import org.jivesoftware.smackx.packet.OfflineMessageInfo;
+import org.jivesoftware.smackx.packet.OfflineMessageRequest;
+import org.jivesoftware.smackx.packet.SharedGroupsInfo;
+import org.jivesoftware.smackx.provider.AdHocCommandDataProvider;
+import org.jivesoftware.smackx.provider.BytestreamsProvider;
+import org.jivesoftware.smackx.provider.DataFormProvider;
+import org.jivesoftware.smackx.provider.DelayInformationProvider;
+import org.jivesoftware.smackx.provider.DiscoverInfoProvider;
+import org.jivesoftware.smackx.provider.DiscoverItemsProvider;
+import org.jivesoftware.smackx.provider.IBBProviders;
+import org.jivesoftware.smackx.provider.MUCAdminProvider;
+import org.jivesoftware.smackx.provider.MUCOwnerProvider;
+import org.jivesoftware.smackx.provider.MUCUserProvider;
+import org.jivesoftware.smackx.provider.MessageEventProvider;
+import org.jivesoftware.smackx.provider.MultipleAddressesProvider;
+import org.jivesoftware.smackx.provider.RosterExchangeProvider;
+import org.jivesoftware.smackx.provider.StreamInitiationProvider;
+import org.jivesoftware.smackx.provider.VCardProvider;
+import org.jivesoftware.smackx.provider.XHTMLExtensionProvider;
+import org.jivesoftware.smackx.search.UserSearch;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.EditText;
+
+public class ChatHandler {
+
+	private XMPPConnection connection;
+	private String serverName;
+	private MultiUserChat chat;
+	private String mainUserName;
+	private boolean chatStarted = false;
+	
+	public ChatHandler(){
+		configure();
+	}
+		
+	public void configure(){
+		configure(ProviderManager.getInstance());
+		SmackConfiguration.setPacketReplyTimeout(10000);
+	}
+	
+	public boolean connect(String server, int port){
+		ConnectionConfiguration config = new ConnectionConfiguration(server,port);
+		config.setSASLAuthenticationEnabled(true);
+		connection = new XMPPConnection(config); //connects to server address provided
+		try {
+			connection.connect();
+			serverName = server;
+			return true;
+		} catch (XMPPException e) {
+			Log.e("PHAIL", "connection exception occurred at connection.connect()", e);
+			return false;
+		}
+	}
+	
+	public boolean login(String userName, String password){
+		try {
+			connection.login(userName, password);
+			mainUserName = userName;
+			return true;
+		} catch (XMPPException e) {
+			return false;
+		}
+	}
+	
+	public boolean createRoom(){
+		chat = new MultiUserChat(connection, "OCtestRoom@" + "conference.cuopencomm");
+		try {
+			chat.join("OCtest");
+			chat.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+		} catch (XMPPException e) {
+			return false;
+		}
+		PacketListener packetListener = new PacketListener() {
+		public void processPacket(Packet arg0) {
+			if(arg0 instanceof Message){
+				Message message = (Message) arg0;
+				String from = message.getFrom();
+				String messageBody = message.getBody();
+				from = from.replace("octestroom@conference.cuopencomm/", "");
+			}
+		}
+		};
+		chat.addMessageListener(packetListener);
+		return true;
+	}
+
+	
+	public void invite(String user){
+		chat.invite(user + "@cuopencomm", "Let's chat!");
+	}
+	
+	public void kickout(String user){
+		
+	}
+
+	public ArrayList<String> buddyList(){
+		ArrayList<String> buddyList = new ArrayList<String>();
+		Collection<RosterEntry> entries = connection.getRoster().getEntries();
+		for (RosterEntry entry : entries) {
+		    buddyList.add(entry.getUser());
+		}
+		return buddyList;
+	}
+	
+	public boolean send(String message){
+		try {
+			chat.sendMessage(message);
+			return true;
+		} catch (XMPPException e) {
+			return false;
+		}
+	}
+	
+	public ArrayList<String> participantsList(){
+		ArrayList<String> participants = new ArrayList<String>();
+		try {
+			Collection<Occupant> occupantEntries = chat.getParticipants();
+			for (Occupant occupant : occupantEntries) {
+			    participants.add(occupant.getJid());
+			}
+			return participants;
+		} catch (XMPPException e) {
+			return null;
+		}
+		
+		
+	}
+	
+	public void logout(){
+		connection.disconnect();
+	}
+	
+
+		public void configure(ProviderManager pm) {
+			// Private Data Storage
+			pm.addIQProvider("query", "jabber:iq:private",
+			new PrivateDataManager.PrivateDataIQProvider());
+
+			// Time
+			try {
+			pm.addIQProvider("query", "jabber:iq:time",
+			Class.forName("org.jivesoftware.smackx.packet.Time"));
+			} catch (ClassNotFoundException e) {
+			Log.w("TestClient",
+			"Can't load class for org.jivesoftware.smackx.packet.Time");
+			}
+
+			// Roster Exchange
+			pm.addExtensionProvider("x", "jabber:x:roster",
+			new RosterExchangeProvider());
+
+			// Message Events
+			pm.addExtensionProvider("x", "jabber:x:event",
+			new MessageEventProvider());
+
+			// Chat State
+			pm.addExtensionProvider("active",
+			"http://jabber.org/protocol/chatstates",
+			new ChatStateExtension.Provider());
+
+			pm.addExtensionProvider("composing",
+			"http://jabber.org/protocol/chatstates",
+			new ChatStateExtension.Provider());
+
+			pm.addExtensionProvider("paused",
+			"http://jabber.org/protocol/chatstates",
+			new ChatStateExtension.Provider());
+
+			pm.addExtensionProvider("inactive",
+			"http://jabber.org/protocol/chatstates",
+			new ChatStateExtension.Provider());
+
+			pm.addExtensionProvider("gone",
+			"http://jabber.org/protocol/chatstates",
+			new ChatStateExtension.Provider());
+
+			// XHTML
+			pm.addExtensionProvider("html", "http://jabber.org/protocol/xhtml-im",
+			new XHTMLExtensionProvider());
+
+			// Group Chat Invitations
+			pm.addExtensionProvider("x", "jabber:x:conference",
+			new GroupChatInvitation.Provider());
+
+			// Service Discovery # Items
+			pm.addIQProvider("query", "http://jabber.org/protocol/disco#items",
+			new DiscoverItemsProvider());
+
+			// Service Discovery # Info
+			pm.addIQProvider("query", "http://jabber.org/protocol/disco#info",
+			new DiscoverInfoProvider());
+
+			// Data Forms
+			pm.addExtensionProvider("x", "jabber:x:data", new DataFormProvider());
+
+			// MUC User
+			pm.addExtensionProvider("x", "http://jabber.org/protocol/muc#user",
+			new MUCUserProvider());
+
+			// MUC Admin
+			pm.addIQProvider("query", "http://jabber.org/protocol/muc#admin",
+			new MUCAdminProvider());
+
+			// MUC Owner
+			pm.addIQProvider("query", "http://jabber.org/protocol/muc#owner",
+			new MUCOwnerProvider());
+
+			// Delayed Delivery
+			pm.addExtensionProvider("x", "jabber:x:delay",
+			new DelayInformationProvider());
+
+			// Version
+			try {
+			pm.addIQProvider("query", "jabber:iq:version",
+			Class.forName("org.jivesoftware.smackx.packet.Version"));
+			} catch (ClassNotFoundException e) {
+			// Not sure what's happening here.
+			}
+
+			// VCard
+			pm.addIQProvider("vCard", "vcard-temp", new VCardProvider());
+
+			// Offline Message Requests
+			pm.addIQProvider("offline", "http://jabber.org/protocol/offline",
+			new OfflineMessageRequest.Provider());
+
+			// Offline Message Indicator
+			pm.addExtensionProvider("offline",
+			"http://jabber.org/protocol/offline",
+			new OfflineMessageInfo.Provider());
+
+			// Last Activity
+			pm.addIQProvider("query", "jabber:iq:last", new LastActivity.Provider());
+
+			// User Search
+			pm.addIQProvider("query", "jabber:iq:search", new UserSearch.Provider());
+
+			// SharedGroupsInfo
+			pm.addIQProvider("sharedgroup",
+			"http://www.jivesoftware.org/protocol/sharedgroup",
+			new SharedGroupsInfo.Provider());
+
+			// JEP-33: Extended Stanza Addressing
+			pm.addExtensionProvider("addresses",
+			"http://jabber.org/protocol/address",
+			new MultipleAddressesProvider());
+
+			// FileTransfer
+			pm.addIQProvider("si", "http://jabber.org/protocol/si",
+			new StreamInitiationProvider());
+
+			// [TODO] : Ask Kris
+			// pm.addIQProvider("query","http://jabber.org/protocol/bytestreams",
+			// new BytestreamsProvider());
+			// pm.addIQProvider("open","http://jabber.org/protocol/ibb", new
+			// IBBProviders.Open());
+			// pm.addIQProvider("close","http://jabber.org/protocol/ibb", new
+			// IBBProviders.Close());
+			// pm.addExtensionProvider("data","http://jabber.org/protocol/ibb", new
+			// IBBProviders.Data());
+
+			// Privacy
+			pm.addIQProvider("query", "jabber:iq:privacy", new PrivacyProvider());
+
+			pm.addIQProvider("command", "http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider());
+			pm.addExtensionProvider("malformed-action",
+			"http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider.MalformedActionError());
+			pm.addExtensionProvider("bad-locale",
+			"http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider.BadLocaleError());
+			pm.addExtensionProvider("bad-payload",
+			"http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider.BadPayloadError());
+			pm.addExtensionProvider("bad-sessionid",
+			"http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider.BadSessionIDError());
+			pm.addExtensionProvider("session-expired",
+			"http://jabber.org/protocol/commands",
+			new AdHocCommandDataProvider.SessionExpiredError());
+    }
+}
+	
+
---
+++ Spring2013/Training Projects/WorkingDemo/src/com/example/view/Login.java
@@ -0,0 +1,94 @@
+package com.example.view;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import com.example.guitest.R;
+import com.example.view.ChatHandler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+
+public class Login extends Activity 
+{
+	private EditText usernameEdit;
+	private EditText passwordEdit;
+	private Button loginButton;
+	private final String SERVER_NAME = "cuopencomm.no-ip.org";
+	private final int PORT = 5222;
+	static ChatHandler chat = new com.example.view.ChatHandler();
+	private boolean connected;
+	private boolean loggedIn;
+	private boolean createdRoom;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		usernameEdit = (EditText)findViewById(R.id.tbx_usrn);
+		passwordEdit = (EditText)findViewById(R.id.tbx_pwd);
+		loginButton = (Button)findViewById(R.id.btn_signin);
+		loginButton.setOnClickListener(listener);
+		connected = chat.connect(SERVER_NAME, PORT);
+	}
+
+	
+	View.OnClickListener listener=new OnClickListener() 
+	{
+		
+		public void onClick(View v) {
+			switch(v.getId())
+			{
+			//case R.id.:
+				//
+				//break;
+				
+			case R.id.btn_signin:
+				//
+				String username,password; 
+				username = usernameEdit.getText().toString().trim();
+				password = passwordEdit.getText().toString().trim();
+				
+				if(username == null || username.equals("")||password == null || password.equals(""))
+				{
+					Toast.makeText(Login.this, "No input text!", Toast.LENGTH_SHORT).show();
+					break;
+				}
+				//else:
+				//get login time for future use
+				Date now=new Date(System.currentTimeMillis());
+				SimpleDateFormat format=new SimpleDateFormat("hh:mm:ss");
+				String nowStr=format.format(now);
+				
+				Toast.makeText(Login.this, "Logging in", Toast.LENGTH_SHORT).show();
+				System.out.println(connected);
+				loggedIn = chat.login(username, password);
+				if(loggedIn && connected)
+				{
+					Toast.makeText(Login.this, "Successful!", Toast.LENGTH_SHORT).show();
+					createdRoom = chat.createRoom();
+					startActivity(new Intent(Login.this, ChatRoom.class));
+				}
+				
+				
+				usernameEdit.setText("");
+				passwordEdit.setText("");
+				break;
+				
+			//case R.id.leave:
+			//	break;
+			}
+		}
+	};
+}
+
