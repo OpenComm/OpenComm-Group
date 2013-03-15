c09c757ed8498b60686df267007222905704573f
unknown <hy323@csugcis02403.csuglab.cornell.edu>
2013-03-15 15:50:27 -0400

    added demo project

---
+++ Spring2013/Training Projects/Demo2/src/com/example/demo2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.demo2;
+import java.io.IOException;
+import java.io.Serializable;
+
+import network.NetConnection;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	NetConnection nc;
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy); 		
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() 
+		{			
+					@Override
+					public void onClick(View v) {
+						try
+						{
+							nc = new NetConnection();
+							nc.connect();
+							nc.login();
+							System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+							Intent intent = new Intent(MainActivity.this, ChatRoom.class);
+//							Bundle b= new Bundle();
+//						    b.putSerializable("network.NetConnection", (Serializable) nc);
+//						    intent.putExtras(b);
+						    startActivity(intent);
+						}
+						catch(Exception e)
+						{
+							e.printStackTrace();
+							System.out.println("sth is wrong");
+						}
+					}
+				});
+	}	
+}
---
+++ Spring2013/Training Projects/Demo2/src/model/ModelChatRoom.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.util.*;
+import org.jivesoftware.smack.Connection;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.muc.Affiliate;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
+public class ModelChatRoom {
+	public static final String DEFAULT_HOSTNAME = "@cuopencomm";
+	MultiUserChat muc;
+	ArrayList<MultiUserChat> musl = new ArrayList<MultiUserChat>();
+	XMPPConnection conn;
+	public ModelChatRoom(Connection conn)
+	{
+		this.conn = (XMPPConnection) conn;
+	}
+	public boolean makeRoom(String rName)
+	{
+		muc = new MultiUserChat(conn, rName+DEFAULT_HOSTNAME);
+		musl.add(muc);
+		try
+		{
+			muc.join("NICKNAME");
+		}
+		catch(XMPPException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	public void banUser(String jid, String reason)
+	{
+		try 
+		{
+			muc.banUser(jid, reason);
+		} 
+		catch (XMPPException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	public void inviteUser(String jid, String reason)
+	{
+		muc.invite(jid, reason);
+	}
+	public String printUserList()
+	{
+		Collection<Affiliate> memList;
+		try {
+			memList = muc.getMembers();
+		} catch (XMPPException e) {
+			e.printStackTrace();
+			return "memList cannot be created";
+		}
+		String memIDs="";
+		for(Affiliate a : memList)
+		{
+			memIDs += (a.getJid() +" / ");
+		}
+		return memIDs;
+	}
+	public boolean sendMessage(String mes)
+	{
+		try
+		{
+			muc.sendMessage(mes);
+			return true; //if successful
+		}
+		catch(XMPPException e)
+		{
+			return false; // if unsuccessful
+		}
+	}
+	public MultiUserChat fetchRoom(String rName)
+	{
+		//we may fetch a room according to the rName
+		return muc;
+	}
+	
+	
+	
+}
