aaf6728c3392efe2d9d5a8291651e8034ca5da97
lucilley <ly77@cornell.edu>
2013-03-16 00:07:06 -0400

    Forgot Password

---
+++ Spring2013/Cycle_1/OC-AndroidApp/src/edu/cornell/opencomm/controller/ForgotPsdController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import com.example.opencomm.R;
+
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.view.View;
+import android.widget.Button;
+import view.ForgotPsdView;
+
+
+/**
+  * Controller for forgot password screen. 
+  * @author Lu Yang[frontend]
+  */
+
+public class ForgotPsdController {
+	
+	private ForgotPsdView forgotPsdView;
+	
+	public ForgotPsdController(ForgotPsdView view){
+		this.forgotPsdView = view;
+	}
+	
+	public void handleSendEmailClick(Email email){
+			//Implement Here
+		}
+		
+}
+	
+
---
+++ Spring2013/Cycle_1/OC-AndroidApp/src/edu/cornell/opencomm/view/ForgotPsdView.java
@@ -0,0 +1,39 @@
+package view;
+
+import com.example.opencomm.R;
+
+import controller.ForgotPsdController;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+/**
+  * View for forgot password screen. 
+  * @author Lu Yang[frontend]
+  */
+
+public class ForgotPsdView extends Activity {
+	
+	private static final View ImageView = null;
+	private ForgotPsdController forgotPsdController;
+	private EditText emailEdit;
+	private ImageView sendEmail;
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.forgot_password);
+		emailEdit= (EditText) findViewById(R.id.email_input);
+		sendEmail = (ImageView) findViewById(R.id.send_email_button);
+		forgotPsdController=new ForgotPsdController(this);
+	}
+	
+	public void sendEmail(View v){
+		Button button = (Button)findViewById(R.id.send_email_button);
+		button.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		        // Do something in response to button click
+		    }
+		});		
+	}
+}
