539121a766e0b0ea941ea3450ba4883fbf5a0e7b
Alin Barsan <alinbarsan@comcast.net>
2013-03-24 11:07:42 -0400

    fixed + finalized working demo

--- Spring2013/Training Projects/WorkingDemo/src/com/example/view/ChatHandler.java
+++ Spring2013/Training Projects/WorkingDemo/src/com/example/view/ChatHandler.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.PacketListener;
-import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.SmackConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
@@ -25,12 +24,10 @@ import org.jivesoftware.smackx.packet.OfflineMessageInfo;
 import org.jivesoftware.smackx.packet.OfflineMessageRequest;
 import org.jivesoftware.smackx.packet.SharedGroupsInfo;
 import org.jivesoftware.smackx.provider.AdHocCommandDataProvider;
-import org.jivesoftware.smackx.provider.BytestreamsProvider;
 import org.jivesoftware.smackx.provider.DataFormProvider;
 import org.jivesoftware.smackx.provider.DelayInformationProvider;
 import org.jivesoftware.smackx.provider.DiscoverInfoProvider;
 import org.jivesoftware.smackx.provider.DiscoverItemsProvider;
-import org.jivesoftware.smackx.provider.IBBProviders;
 import org.jivesoftware.smackx.provider.MUCAdminProvider;
 import org.jivesoftware.smackx.provider.MUCOwnerProvider;
 import org.jivesoftware.smackx.provider.MUCUserProvider;
@@ -42,269 +39,330 @@ import org.jivesoftware.smackx.provider.VCardProvider;
 import org.jivesoftware.smackx.provider.XHTMLExtensionProvider;
 import org.jivesoftware.smackx.search.UserSearch;
 
-import android.content.Context;
 import android.util.Log;
-import android.widget.EditText;
 
 public class ChatHandler {
 
 	private XMPPConnection connection;
-	private String serverName;
+	private static final boolean D = true;
+	// set boolean to false to disable debugging log statements
+	private static final String TAG = "ChatHandler";
 	private MultiUserChat chat;
-	private String mainUserName;
-	private boolean chatStarted = false;
-	
-	public ChatHandler(){
+
+	public ChatHandler() {
 		configure();
+		// configures the provider managers upon instantiation
 	}
-		
-	public void configure(){
+
+	
+	
+	public void configure() {
 		configure(ProviderManager.getInstance());
 		SmackConfiguration.setPacketReplyTimeout(10000);
 	}
 	
-	public boolean connect(String server, int port){
-		ConnectionConfiguration config = new ConnectionConfiguration(server,port);
+	
+	public boolean connect(String server, int port) {
+		ConnectionConfiguration config = new ConnectionConfiguration(server,
+				port);
 		config.setSASLAuthenticationEnabled(true);
-		connection = new XMPPConnection(config); //connects to server address provided
+		connection = new XMPPConnection(config);
+		// configures the connection and establishes it. Returns false if failed
 		try {
 			connection.connect();
-			serverName = server;
+			if (D)
+				Log.v(TAG, "connection successful");
 			return true;
 		} catch (XMPPException e) {
-			Log.e("PHAIL", "connection exception occurred at connection.connect()", e);
+			if (D)
+				Log.v(TAG,
+						"connection exception occurred at connection.connect()");
 			return false;
 		}
 	}
-	
-	public boolean login(String userName, String password){
+
+	public boolean login(String userName, String password) {
 		try {
 			connection.login(userName, password);
-			mainUserName = userName;
+			// logs in with username and password, returns false if failed
+			if (D)
+				Log.v(TAG, "login successful");
 			return true;
 		} catch (XMPPException e) {
+			if (D)
+				Log.v(TAG, "login failed");
 			return false;
 		}
 	}
-	
-	public boolean createRoom(){
-		chat = new MultiUserChat(connection, "OCtestRoom@" + "conference.cuopencomm");
+
+	public boolean createRoom() {
+		chat = new MultiUserChat(connection, "OCtestRoom@"
+				+ "conference.cuopencomm");
+		// instantiates the MUC
 		try {
 			chat.join("OCtest");
+			// creates chat room by attempting to join a nonexistant one, makes
+			// a default room. Configures it to default
 			chat.sendConfigurationForm(new Form(Form.TYPE_SUBMIT));
+			if (D)
+				Log.v(TAG, "MUC successfully created");
 		} catch (XMPPException e) {
+			if (D)
+				Log.v(TAG, "failed to create MUC");
 			return false;
 		}
 		PacketListener packetListener = new PacketListener() {
-		public void processPacket(Packet arg0) {
-			if(arg0 instanceof Message){
+			// function for processing the packets. Assumes that they are all
+			// messages since it is added as a message listener
+			public void processPacket(Packet arg0) {
 				Message message = (Message) arg0;
 				String from = message.getFrom();
 				String messageBody = message.getBody();
 				from = from.replace("octestroom@conference.cuopencomm/", "");
+				// retrieves and configures the information from the message
+				String fullMessage = from + ":  " + messageBody;
+				synchronized(ChatRoom.messages) {
+				ChatRoom.messages.add(fullMessage);
+				}
 			}
-		}
 		};
 		chat.addMessageListener(packetListener);
+		// adds the message listener
 		return true;
 	}
 
-	
-	public void invite(String user){
-		chat.invite(user + "@cuopencomm", "Let's chat!");
-	}
-	
-	public void kickout(String user){
-		
+	public void invite(String user) {
+		chat.invite(user, "let's chat");
+		// sends a simple invitation, does not listen for rejections/acceptance.
+		// Note that this function uses the form "oc2testorg@cuopencomm"
+		if (D)
+			Log.v(TAG, "invitation sent to user: " + user);
 	}
 
-	public ArrayList<String> buddyList(){
-		ArrayList<String> buddyList = new ArrayList<String>();
-		Collection<RosterEntry> entries = connection.getRoster().getEntries();
-		for (RosterEntry entry : entries) {
-		    buddyList.add(entry.getUser());
-		}
-		return buddyList;
-	}
-	
-	public boolean send(String message){
+	public void kickout(String user) {
 		try {
-			chat.sendMessage(message);
-			return true;
+			chat.kickParticipant(user, "I dont need a reason");
+			// kicks the specified user. Note that this function uses the form
+			// "oc2testorg" unlike the invite function
+			if (D)
+				Log.v(TAG, "kickout function called");
 		} catch (XMPPException e) {
-			return false;
+			if (D)
+				Log.v(TAG, "failed to kick participant");
 		}
 	}
-	
-	public ArrayList<String> participantsList(){
-		ArrayList<String> participants = new ArrayList<String>();
+
+	public ArrayList<String> getParticipants() {
+		Collection<Occupant> occupants = null;
 		try {
-			Collection<Occupant> occupantEntries = chat.getParticipants();
-			for (Occupant occupant : occupantEntries) {
-			    participants.add(occupant.getJid());
-			}
-			return participants;
+			occupants = chat.getParticipants();
+			//retreives participants
+			if (D)
+				Log.v(TAG, "Retreived MUC participants");
 		} catch (XMPPException e) {
+			if (D)
+				Log.v(TAG, "Failed to retrieve participants of the MUC");
 			return null;
 		}
-		
-		
+		if (D)
+			Log.v(TAG, "Participant List:");
+		ArrayList<String> participants = new ArrayList<String>();
+		for (Occupant occupant : occupants) {
+			//retrieves and stores the user info as a string
+			String part = occupant.getJid();
+			int cutoff = part.indexOf("/");
+			part = part.substring(0, cutoff);
+			participants.add(part);
+			if (D)
+				Log.v(TAG, part);
+		}
+
+		return participants;
 	}
-	
-	public void logout(){
+
+	public void send(String message) {
+		try {
+			chat.sendMessage(message);
+			//sends a message to the chat, messagelistener will catch this
+		} catch (XMPPException e) {
+			if (D)
+				Log.v(TAG, "failed to send message");
+		}
+	}
+
+	public ArrayList<String> getRoster() {
+		ArrayList<String> buddies = new ArrayList<String>();
+		Collection<RosterEntry> entries = connection.getRoster().getEntries();
+		//gets buddies as RosterEntries
+		if (D)
+			Log.v(TAG, "Buddylist:");
+		for (RosterEntry entry : entries) {
+			buddies.add(entry.getUser());
+			//stores the entries as strings representing user names
+			if (D)
+				Log.v(TAG, entry.getUser());
+		}
+
+		return buddies;
+	}
+
+	public void logout() {
 		connection.disconnect();
+		//severs the connection
+		if (D)
+			Log.v(TAG, "disconnected");
 	}
-	
 
-		public void configure(ProviderManager pm) {
-			// Private Data Storage
-			pm.addIQProvider("query", "jabber:iq:private",
-			new PrivateDataManager.PrivateDataIQProvider());
+	public void configure(ProviderManager pm) {
+		//configuration method.
+		if (D)
+			Log.v(TAG, "configuring provider managers...");
+		// Private Data Storage
+		pm.addIQProvider("query", "jabber:iq:private",
+				new PrivateDataManager.PrivateDataIQProvider());
 
-			// Time
-			try {
+		// Time
+		try {
 			pm.addIQProvider("query", "jabber:iq:time",
-			Class.forName("org.jivesoftware.smackx.packet.Time"));
-			} catch (ClassNotFoundException e) {
+					Class.forName("org.jivesoftware.smackx.packet.Time"));
+		} catch (ClassNotFoundException e) {
 			Log.w("TestClient",
-			"Can't load class for org.jivesoftware.smackx.packet.Time");
-			}
+					"Can't load class for org.jivesoftware.smackx.packet.Time");
+		}
 
-			// Roster Exchange
-			pm.addExtensionProvider("x", "jabber:x:roster",
-			new RosterExchangeProvider());
+		// Roster Exchange
+		pm.addExtensionProvider("x", "jabber:x:roster",
+				new RosterExchangeProvider());
 
-			// Message Events
-			pm.addExtensionProvider("x", "jabber:x:event",
-			new MessageEventProvider());
+		// Message Events
+		pm.addExtensionProvider("x", "jabber:x:event",
+				new MessageEventProvider());
 
-			// Chat State
-			pm.addExtensionProvider("active",
-			"http://jabber.org/protocol/chatstates",
-			new ChatStateExtension.Provider());
+		// Chat State
+		pm.addExtensionProvider("active",
+				"http://jabber.org/protocol/chatstates",
+				new ChatStateExtension.Provider());
 
-			pm.addExtensionProvider("composing",
-			"http://jabber.org/protocol/chatstates",
-			new ChatStateExtension.Provider());
+		pm.addExtensionProvider("composing",
+				"http://jabber.org/protocol/chatstates",
+				new ChatStateExtension.Provider());
 
-			pm.addExtensionProvider("paused",
-			"http://jabber.org/protocol/chatstates",
-			new ChatStateExtension.Provider());
+		pm.addExtensionProvider("paused",
+				"http://jabber.org/protocol/chatstates",
+				new ChatStateExtension.Provider());
 
-			pm.addExtensionProvider("inactive",
-			"http://jabber.org/protocol/chatstates",
-			new ChatStateExtension.Provider());
+		pm.addExtensionProvider("inactive",
+				"http://jabber.org/protocol/chatstates",
+				new ChatStateExtension.Provider());
 
-			pm.addExtensionProvider("gone",
-			"http://jabber.org/protocol/chatstates",
-			new ChatStateExtension.Provider());
+		pm.addExtensionProvider("gone",
+				"http://jabber.org/protocol/chatstates",
+				new ChatStateExtension.Provider());
 
-			// XHTML
-			pm.addExtensionProvider("html", "http://jabber.org/protocol/xhtml-im",
-			new XHTMLExtensionProvider());
+		// XHTML
+		pm.addExtensionProvider("html", "http://jabber.org/protocol/xhtml-im",
+				new XHTMLExtensionProvider());
 
-			// Group Chat Invitations
-			pm.addExtensionProvider("x", "jabber:x:conference",
-			new GroupChatInvitation.Provider());
+		// Group Chat Invitations
+		pm.addExtensionProvider("x", "jabber:x:conference",
+				new GroupChatInvitation.Provider());
 
-			// Service Discovery # Items
-			pm.addIQProvider("query", "http://jabber.org/protocol/disco#items",
-			new DiscoverItemsProvider());
+		// Service Discovery # Items
+		pm.addIQProvider("query", "http://jabber.org/protocol/disco#items",
+				new DiscoverItemsProvider());
 
-			// Service Discovery # Info
-			pm.addIQProvider("query", "http://jabber.org/protocol/disco#info",
-			new DiscoverInfoProvider());
+		// Service Discovery # Info
+		pm.addIQProvider("query", "http://jabber.org/protocol/disco#info",
+				new DiscoverInfoProvider());
 
-			// Data Forms
-			pm.addExtensionProvider("x", "jabber:x:data", new DataFormProvider());
+		// Data Forms
+		pm.addExtensionProvider("x", "jabber:x:data", new DataFormProvider());
 
-			// MUC User
-			pm.addExtensionProvider("x", "http://jabber.org/protocol/muc#user",
-			new MUCUserProvider());
+		// MUC User
+		pm.addExtensionProvider("x", "http://jabber.org/protocol/muc#user",
+				new MUCUserProvider());
 
-			// MUC Admin
-			pm.addIQProvider("query", "http://jabber.org/protocol/muc#admin",
-			new MUCAdminProvider());
+		// MUC Admin
+		pm.addIQProvider("query", "http://jabber.org/protocol/muc#admin",
+				new MUCAdminProvider());
 
-			// MUC Owner
-			pm.addIQProvider("query", "http://jabber.org/protocol/muc#owner",
-			new MUCOwnerProvider());
+		// MUC Owner
+		pm.addIQProvider("query", "http://jabber.org/protocol/muc#owner",
+				new MUCOwnerProvider());
 
-			// Delayed Delivery
-			pm.addExtensionProvider("x", "jabber:x:delay",
-			new DelayInformationProvider());
+		// Delayed Delivery
+		pm.addExtensionProvider("x", "jabber:x:delay",
+				new DelayInformationProvider());
 
-			// Version
-			try {
+		// Version
+		try {
 			pm.addIQProvider("query", "jabber:iq:version",
-			Class.forName("org.jivesoftware.smackx.packet.Version"));
-			} catch (ClassNotFoundException e) {
+					Class.forName("org.jivesoftware.smackx.packet.Version"));
+		} catch (ClassNotFoundException e) {
 			// Not sure what's happening here.
-			}
+		}
 
-			// VCard
-			pm.addIQProvider("vCard", "vcard-temp", new VCardProvider());
+		// VCard
+		pm.addIQProvider("vCard", "vcard-temp", new VCardProvider());
 
-			// Offline Message Requests
-			pm.addIQProvider("offline", "http://jabber.org/protocol/offline",
-			new OfflineMessageRequest.Provider());
+		// Offline Message Requests
+		pm.addIQProvider("offline", "http://jabber.org/protocol/offline",
+				new OfflineMessageRequest.Provider());
 
-			// Offline Message Indicator
-			pm.addExtensionProvider("offline",
-			"http://jabber.org/protocol/offline",
-			new OfflineMessageInfo.Provider());
+		// Offline Message Indicator
+		pm.addExtensionProvider("offline",
+				"http://jabber.org/protocol/offline",
+				new OfflineMessageInfo.Provider());
 
-			// Last Activity
-			pm.addIQProvider("query", "jabber:iq:last", new LastActivity.Provider());
+		// Last Activity
+		pm.addIQProvider("query", "jabber:iq:last", new LastActivity.Provider());
 
-			// User Search
-			pm.addIQProvider("query", "jabber:iq:search", new UserSearch.Provider());
+		// User Search
+		pm.addIQProvider("query", "jabber:iq:search", new UserSearch.Provider());
 
-			// SharedGroupsInfo
-			pm.addIQProvider("sharedgroup",
-			"http://www.jivesoftware.org/protocol/sharedgroup",
-			new SharedGroupsInfo.Provider());
+		// SharedGroupsInfo
+		pm.addIQProvider("sharedgroup",
+				"http://www.jivesoftware.org/protocol/sharedgroup",
+				new SharedGroupsInfo.Provider());
 
-			// JEP-33: Extended Stanza Addressing
-			pm.addExtensionProvider("addresses",
-			"http://jabber.org/protocol/address",
-			new MultipleAddressesProvider());
+		// JEP-33: Extended Stanza Addressing
+		pm.addExtensionProvider("addresses",
+				"http://jabber.org/protocol/address",
+				new MultipleAddressesProvider());
 
-			// FileTransfer
-			pm.addIQProvider("si", "http://jabber.org/protocol/si",
-			new StreamInitiationProvider());
+		// FileTransfer
+		pm.addIQProvider("si", "http://jabber.org/protocol/si",
+				new StreamInitiationProvider());
 
-			// [TODO] : Ask Kris
-			// pm.addIQProvider("query","http://jabber.org/protocol/bytestreams",
-			// new BytestreamsProvider());
-			// pm.addIQProvider("open","http://jabber.org/protocol/ibb", new
-			// IBBProviders.Open());
-			// pm.addIQProvider("close","http://jabber.org/protocol/ibb", new
-			// IBBProviders.Close());
-			// pm.addExtensionProvider("data","http://jabber.org/protocol/ibb", new
-			// IBBProviders.Data());
+		// [TODO] : Ask Kris
+		// pm.addIQProvider("query","http://jabber.org/protocol/bytestreams",
+		// new BytestreamsProvider());
+		// pm.addIQProvider("open","http://jabber.org/protocol/ibb", new
+		// IBBProviders.Open());
+		// pm.addIQProvider("close","http://jabber.org/protocol/ibb", new
+		// IBBProviders.Close());
+		// pm.addExtensionProvider("data","http://jabber.org/protocol/ibb", new
+		// IBBProviders.Data());
 
-			// Privacy
-			pm.addIQProvider("query", "jabber:iq:privacy", new PrivacyProvider());
+		// Privacy
+		pm.addIQProvider("query", "jabber:iq:privacy", new PrivacyProvider());
 
-			pm.addIQProvider("command", "http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider());
-			pm.addExtensionProvider("malformed-action",
-			"http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider.MalformedActionError());
-			pm.addExtensionProvider("bad-locale",
-			"http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider.BadLocaleError());
-			pm.addExtensionProvider("bad-payload",
-			"http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider.BadPayloadError());
-			pm.addExtensionProvider("bad-sessionid",
-			"http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider.BadSessionIDError());
-			pm.addExtensionProvider("session-expired",
-			"http://jabber.org/protocol/commands",
-			new AdHocCommandDataProvider.SessionExpiredError());
-    }
+		pm.addIQProvider("command", "http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider());
+		pm.addExtensionProvider("malformed-action",
+				"http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider.MalformedActionError());
+		pm.addExtensionProvider("bad-locale",
+				"http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider.BadLocaleError());
+		pm.addExtensionProvider("bad-payload",
+				"http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider.BadPayloadError());
+		pm.addExtensionProvider("bad-sessionid",
+				"http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider.BadSessionIDError());
+		pm.addExtensionProvider("session-expired",
+				"http://jabber.org/protocol/commands",
+				new AdHocCommandDataProvider.SessionExpiredError());
+	}
 }
-	
-
--- Spring2013/Training Projects/WorkingDemo/src/com/example/view/ChatRoom.java
+++ Spring2013/Training Projects/WorkingDemo/src/com/example/view/ChatRoom.java
@@ -1,164 +1,170 @@
 package com.example.view;
 
-
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 import com.example.guitest.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
-
-public class ChatRoom extends Activity 
-{
+public class ChatRoom extends Activity {
 	private EditText historyEdit;
 	private EditText messageEdit;
 	private EditText nameEdit;
-	
+
 	private Button sendButton;
 	private Button leaveButton;
 	private Button addButton;
 	private Button delButton;
 	private Button blButton;
 	private Button chButton;
-	//private ListView buddylist = (ListView) findViewById(R.id.BuddyListView);
-	//private ListView chatlist = (ListView) findViewById(R.id.ChattingListView);
-	
-	
+	// private ListView buddylist = (ListView) findViewById(R.id.BuddyListView);
+	// private ListView chatlist = (ListView)
+	// findViewById(R.id.ChattingListView);
+	private boolean messagesAccessible = true;
+	public static LinkedList<String> messages = new LinkedList<String>();
+	private String tempUser;
+	private String tempMess;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_chat_room);
-        historyEdit=(EditText)findViewById(R.id.history);
-        messageEdit=(EditText)findViewById(R.id.message);
-        nameEdit = (EditText)findViewById(R.id.name);
-        
-        sendButton=(Button)findViewById(R.id.send);
-        leaveButton = (Button)findViewById(R.id.leave);
-        addButton = (Button)findViewById(R.id.add);
-        delButton = (Button)findViewById(R.id.del);
-        blButton = (Button)findViewById(R.id.buddyList);
-        chButton = (Button)findViewById(R.id.chatList);
-        
-        sendButton.setOnClickListener(listener);
-        leaveButton.setOnClickListener(listener);
-        addButton.setOnClickListener(listener);
-        delButton.setOnClickListener(listener);
-        blButton.setOnClickListener(listener);
-        chButton.setOnClickListener(listener);
+		historyEdit = (EditText) findViewById(R.id.history);
+		messageEdit = (EditText) findViewById(R.id.message);
+		nameEdit = (EditText) findViewById(R.id.name);
 
-       // buddylist.setAdapter((ListAdapter) new ArrayAdapter<String>(this, android.R.layout.activity_chat_room,getData()));
-    	
+		sendButton = (Button) findViewById(R.id.send);
+		leaveButton = (Button) findViewById(R.id.leave);
+		addButton = (Button) findViewById(R.id.add);
+		delButton = (Button) findViewById(R.id.del);
+		blButton = (Button) findViewById(R.id.buddyList);
+		chButton = (Button) findViewById(R.id.chatList);
+
+		sendButton.setOnClickListener(listener);
+		leaveButton.setOnClickListener(listener);
+		addButton.setOnClickListener(listener);
+		delButton.setOnClickListener(listener);
+		blButton.setOnClickListener(listener);
+		chButton.setOnClickListener(listener);
+
+		PollList pollList = new PollList();
+		pollList.execute(new Object[3]);
+		// buddylist.setAdapter((ListAdapter) new ArrayAdapter<String>(this,
+		// android.R.layout.activity_chat_room,getData()));
+
 	}
-	
-	
-	
-	
-	private void logout()
-	{
+
+	public class PollList extends AsyncTask<Object, Object, Object> {
+		@Override
+		protected void onProgressUpdate(Object... values) {
+			showMessage(tempUser, tempMess);
+		}
+
+		@Override
+		protected Object doInBackground(Object... params) {
+			while (messagesAccessible) {
+				try {
+					Thread.sleep(250);
+				} catch (InterruptedException e) {
+					// thread interrupted
+				}
+				while (!messages.isEmpty()) {
+						String message = messages.poll();
+						int colon = message.indexOf(":");
+						String user = message.substring(0, colon);
+						String messageBody = message.substring(colon + 1);
+						messageBody.trim();
+						tempUser = user;
+						tempMess = messageBody;
+						this.publishProgress(new Object[3]);
+				}
+			}
+
+			return null;
+		}
+	}
+
+	private void logout() {
 		showTost("Logging out");
 		startActivity(new Intent(ChatRoom.this, Login.class));
 	}
 
-	private void showTost(String content)
-	{
+	private void showTost(String content) {
 		Toast.makeText(ChatRoom.this, content, Toast.LENGTH_SHORT).show();
 	}
-	
-	private void showList(ArrayList<String> list)
-	{
+
+	private void showList(ArrayList<String> list) {
 		String Content = "";
-		for(int n = 0 ; n < list.size()-1; n++)
-		{
-			Content += list.get(n)+"\n";
+		for (int n = 0; n < list.size(); n++) {
+			Content += list.get(n) + "\n";
 		}
-		Content += list.get(list.size()-1);
-		showTost(Content);
+		if (Content.equals("")) {
+			showTost("There are no other participants in your chat room");
+		} else {
+			showTost(Content);
+		}
 	}
-	
-	View.OnClickListener listener=new OnClickListener() 
-	{
-		
+
+	View.OnClickListener listener = new OnClickListener() {
+
 		public void onClick(View v) {
-			switch(v.getId())
-			{
+			switch (v.getId()) {
 			case R.id.leave:
 				Login.chat.logout();
 				logout();
 				break;
-				
+
 			case R.id.send:
 				//
-				String chat_txt; 
-				chat_txt=messageEdit.getText().toString().trim();
-				if(chat_txt == null || chat_txt.equals(""))
-				{
+				String chat_txt;
+				chat_txt = messageEdit.getText().toString().trim();
+				if (chat_txt == null || chat_txt.equals("")) {
 					showTost("No input text!");
 					break;
 				}
 				Login.chat.send(chat_txt);
 				messageEdit.setText("");
-				showMessage("localhost",chat_txt);
 				break;
-		
+
 			case R.id.buddyList:
-				// TODO: get the buddylist(online list) in ArrayList<String>
-				// and show them up with :  showList(ArrayList<String> list) here
-				//showList(list);
-				showList(Login.chat.buddyList());
-			break;
-			
+				showList(Login.chat.getRoster());
+				break;
+
 			case R.id.chatList:
-				// TODO: get the chatlist in ArrayList<String>
-				// and show them up with : showList(ArrayList<String> list) here
-				//showList(list);
-				showList(Login.chat.participantsList());
-			break;
-			
-			
+				showList(Login.chat.getParticipants());
+				break;
+
 			case R.id.add:
-				// TODO: add some one to chat list (invite somebody to chat)
 				String name = nameEdit.getText().toString().trim();
 				Login.chat.invite(name);
 				showTost("done!");
-			break;
-			
+				break;
+
 			case R.id.del:
-				// TODO: delete some one to chat list (kick out somebody)
-				//String name = nameEdit.getText().toString().trim();
-				//delete name
-				//showTost("done!");
-			break;
+				String name2 = nameEdit.getText().toString().trim();
+				Login.chat.kickout(name2);
+				break;
 			}
 		}
 	};
-	
-	
-	//TODO: Show messages. When catching a coming message
-	// you can use it to show in the GUI
-	private void showMessage(String name, String message)
-	{
-		Date now=new Date(System.currentTimeMillis());
-		SimpleDateFormat format=new SimpleDateFormat("hh:mm:ss");
-		String nowStr=format.format(now);
-		historyEdit.append(name+"("+nowStr+"): "+ message +"\n");
+
+	private void showMessage(String name, String message) {
+		Date now = new Date(System.currentTimeMillis());
+		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+		String nowStr = format.format(now);
+		historyEdit.append(name + "(" + nowStr + "): " + message + "\n");
 		messageEdit.setText("");
 	}
 }
-
-
