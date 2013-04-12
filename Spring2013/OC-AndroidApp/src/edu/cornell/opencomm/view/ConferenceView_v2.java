a26309297c0ce26ccb2031352fb92c6ee6a4c26d
Shuai Lu <sl988@cornell.edu>
2013-04-12 15:56:39 -0400

    add fonts files

--- Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/view/ConferenceView_v2.java
+++ Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/view/ConferenceView_v2.java
@@ -2,6 +2,7 @@ package edu.cornell.opencomm.view;
 
 import edu.cornell.opencomm.R;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,6 +31,12 @@ public final class ConferenceView_v2 extends Activity
 		setContentView(R.layout.conference_v2);
 		//instantiate the handlers
 		txtv_ConfTitle = (TextView) findViewById(R.id.confernecev2_title);
+		
+		
+		// Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+        // Applying font
+        txtv_ConfTitle.setTypeface(tf);
 		
 		controller = new ConferenceController_v2(this);
 	}
