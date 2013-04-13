a03f571255ac719fae5e90d16c4af45bb1d2accf
Antoine Chkaiban <antoine.chkaiban@student.ecp.fr>
2013-04-13 15:21:11 -0400

    removed french javadoc...

--- Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/model/InvitationsList.java
+++ Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/model/InvitationsList.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 
 public class InvitationsList {
 	
-	// L'utilisation du mot clé volatile permet, en Java version 5 et supérieur, d'éviter le cas où "Singleton.instance" est non-nul,
-    // mais pas encore "réellement" instancié.
-    // De Java version 1.2 à 1.4, il est possible d'utiliser la classe ThreadLocal.
+
     private static volatile InvitationsList instance = null;
 	
 	private static ArrayList<Invitation> invitationsList = new ArrayList();
@@ -15,8 +13,6 @@ public class InvitationsList {
      * Constructeur de l'objet.
      */
     private InvitationsList() {
-        // La présence d'un constructeur privé supprime le constructeur public par défaut.
-        // De plus, seul le singleton peut s'instancier lui même.
         super();
     }
 
@@ -25,11 +21,9 @@ public class InvitationsList {
      * @return Retourne l'instance du singleton.
      */
     public final static InvitationsList getInstance() {
-        //Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet d'éviter un appel coûteux à synchronized, 
-        //une fois que l'instanciation est faite.
+
         if (InvitationsList.instance == null) {
-           // Le mot-clé synchronized sur ce bloc empêche toute instanciation multiple même par différents "threads".
-           // Il est TRES important.
+
            synchronized(InvitationsList.class) {
              if (InvitationsList.instance == null) {
             	 InvitationsList.instance = new InvitationsList();
