7d933273dd18aa7ae48780bcb424c2e27dff22c5
Kris Kooi <kpk47@cornell.edu>
2013-04-15 15:38:11 -0400

    added OCParticipantStatusListener

---
+++ Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/controller/OCParticipantStatusListener.java
@@ -0,0 +1,108 @@
+package edu.cornell.opencomm.controller;
+
+import org.jivesoftware.smackx.muc.ParticipantStatusListener;
+
+import android.util.Log;
+
+import edu.cornell.opencomm.model.Conference;
+
+public class OCParticipantStatusListener implements ParticipantStatusListener {
+
+	private static final String TAG = OCParticipantStatusListener.class
+			.getName();
+
+	public OCParticipantStatusListener(Conference c) {
+	}
+
+	@Override
+	public void adminGranted(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void adminRevoked(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void banned(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void joined(String arg0) {
+		Log.v(TAG, arg0 + " joined the room");
+		Log.v(TAG, "initiating Jingle session");
+		
+	}
+
+	@Override
+	public void kicked(String arg0, String arg1, String arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void left(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void membershipGranted(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void membershipRevoked(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moderatorGranted(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void moderatorRevoked(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void nicknameChanged(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ownershipGranted(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ownershipRevoked(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void voiceGranted(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void voiceRevoked(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
