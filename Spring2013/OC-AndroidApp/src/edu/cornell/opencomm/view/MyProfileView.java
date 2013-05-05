4dc7c96a1f88646fc0bc5e4754839543f0d16064
lucilley <ly77@cornell.edu>
2013-05-05 15:08:37 -0400

    my profile show user info function

--- Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/view/MyProfileView.java
+++ Spring2013/OC-AndroidApp/src/edu/cornell/opencomm/view/MyProfileView.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import edu.cornell.opencomm.R;
 import edu.cornell.opencomm.controller.MyProfileController;
+import edu.cornell.opencomm.manager.UserManager;
+import edu.cornell.opencomm.model.User;
 
 /**
  * View for my profile screen. 
@@ -18,10 +22,20 @@ public class MyProfileView extends Activity{
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		setUserInfo();
 		setContentView(R.layout.my_profile_layout);
 		myProfileController = new MyProfileController(this);
 		//set font
 		//Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Light.ttf");
+	}
+
+	public void setUserInfo(){
+		User user = UserManager.PRIMARY_USER;
+		if (user == null) System.out.println("no user!");
+		String name = user.getUsername();
+		if (name == null) System.out.println("null!!!");
+		//still need title and cell phone
+		((TextView)findViewById(R.id.my_profile_name_label)).setText(name);
 	}
 	
 	public void back() {
